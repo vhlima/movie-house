5a80bccd7823df15bb824b592eb78b016b6ebb32
Victor <vhlima2000@gmail.com>
2022-06-24 07:20:20 -0300

    ✨ Adds movie carousel card to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next';
 
+import Card from '../components/Card';
+
+import CarouselMovieList from '../components/CarouselMovieList';
+
 import Layout from '../Layout';
 
 const Home: NextPage = () => (
   <Layout>
-    <h1>home</h1>
+    <Card title="Top choices" description="Movies we think you might like">
+      <CarouselMovieList />
+    </Card>
   </Layout>
 );
 
