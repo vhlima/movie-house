9d2ab5b878ece7577d341cfa134e445584449559
Victor <vhlima2000@gmail.com>
2022-06-24 07:15:26 -0300

    🔧 Add image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  images: {
+    domains: ['image.tmdb.org'],
+    formats: ['image/avif', 'image/webp'],
+  },
+};
+
+module.exports = nextConfig;
