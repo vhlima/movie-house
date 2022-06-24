2c525b7ed884a99d65ed5d12443afb5bdb126e9c
Victor <vhlima2000@gmail.com>
2022-06-24 01:57:58 -0300

    🔧 Adds Roboto Mono and UI colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,28 @@ const defaultTheme = require('tailwindcss/defaultTheme'); /* eslint-disable-line
 
 module.exports = {
   content: [
+    './src/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
+      screens: {
+        ...defaultTheme.screens,
+      },
       fontFamily: {
         sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        mono: ['Roboto Mono', ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        ...defaultTheme.colors,
+        background: '#121212',
+        surface: '#121212',
+        complementary: '#000',
+        primary: '#BB86FC',
+        primaryVariant: '#3700B3',
+        secondary: '#dbdbdb',
+        secondaryVariant: '#a8a8a8',
       },
     },
   },
