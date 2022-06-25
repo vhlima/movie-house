247a323a76f73115efe8a96ed5e0d542c227f6ce
Victor <vhlima2000@gmail.com>
2022-06-25 03:21:05 -0300

    💄 Fix problem between modal and footer

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -5,17 +5,17 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => (
-  <div className="relative w-screen h-screen overflow-x-hidden overflow-y-auto font-sans bg-background">
+  <>
     <div id="modalPortal" />
 
-    <div className="flex flex-col">
+    <div className="flex flex-col relative w-screen h-screen overflow-x-hidden overflow-y-auto font-sans bg-background">
       <Navbar />
 
       <div>{children}</div>
 
       <Footer />
     </div>
-  </div>
+  </>
 );
 
 export default Layout;
