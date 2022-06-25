4a9e5eb61f122eb6da278b2e3a980050d8565656
Victor <vhlima2000@gmail.com>
2022-06-25 04:20:45 -0300

    ✨ Adds option to pass link as prop

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,30 +1,55 @@
 import React, { PropsWithChildren } from 'react';
 
+import { FaChevronRight } from 'react-icons/fa';
+
+import Link, { LinkProps } from '../Link';
+
 interface CardProps {
   title: string;
   description?: string;
+  link?: LinkProps;
 }
 
 const Card: React.FC<PropsWithChildren<CardProps>> = ({
   title,
   description,
+  link,
   children,
-}) => (
-  <div className="w-full bg-complementary3">
-    <div className="p-3">
-      <div className="mb-2 select-none">
-        <div className="flex gap-2 items-center">
-          <span className="block w-1 h-6 rounded-lg bg-primary" />
+}) => {
+  const headContainerStyles = 'group';
 
-          <h1 className="text-secondary font-bold text-xl">{title}</h1>
-        </div>
+  const head = (
+    <>
+      <div className="flex gap-2 items-center">
+        <span className="block w-1 h-6 rounded-lg bg-primary" />
 
-        {description && <p className="text-secondaryVariant">{description}</p>}
+        <h1 className="text-secondary font-bold text-xl">{title}</h1>
+
+        <FaChevronRight
+          className="text-secondary group-hover:text-primary"
+          size={20}
+        />
       </div>
 
-      {children}
+      {description && <p className="text-secondaryVariant">{description}</p>}
+    </>
+  );
+
+  return (
+    <div className="w-full bg-complementary3">
+      <div className="flex flex-col gap-2 p-3">
+        {!link ? (
+          <div className={headContainerStyles}>{head}</div>
+        ) : (
+          <Link className={headContainerStyles} {...link}>
+            {head}
+          </Link>
+        )}
+
+        {children}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
