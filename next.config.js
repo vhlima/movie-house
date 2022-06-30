eb4d36703367bfe784e4dbc6b3dbca3ff4159da5
Victor <vhlima2000@gmail.com>
2022-06-30 05:58:44 -0300

    🔧 Adds loremflickr image domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 
   images: {
-    domains: ['image.tmdb.org', 'a.ltrbxd.com', 'www.themoviedb.org'],
+    domains: [
+      'image.tmdb.org',
+      'a.ltrbxd.com',
+      'www.themoviedb.org',
+      'loremflickr.com',
+    ],
     formats: ['image/avif', 'image/webp'],
   },
 };
