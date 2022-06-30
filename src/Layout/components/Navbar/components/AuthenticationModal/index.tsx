c34d1531a755eec83d2ef2a1eba3c3e44ea0f9f9
Victor <vhlima2000@gmail.com>
2022-06-30 06:10:32 -0300

    ✨ Adds backdrop to modal

--- src/Layout/components/Navbar/components/AuthenticationModal/index.tsx
+++ src/Layout/components/Navbar/components/AuthenticationModal/index.tsx
@@ -2,71 +2,86 @@ import React from 'react';
 
 import { Formik, Form } from 'formik';
 
+import { populateJSON } from '../../../../../data/populate';
+
 import { useAuth } from '../../../../../hooks/useAuth';
 
 import Input from '../../../../../components/Input';
 import Button from '../../../../../components/Button';
 import FieldLabel from '../../../../../components/FieldLabel';
+import Backdrop from '../../../../../components/Backdrop';
 
 interface AuthenticationModalProps {
   onSubmit: () => void;
+  onClose: () => void;
 }
 
 const AuthenticationModal: React.FC<AuthenticationModalProps> = ({
   onSubmit,
+  onClose,
 }) => {
   const { user, signIn, signOut } = useAuth();
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (!user) {
-      signIn({ login: 'abc', password: 'abc' });
+      await signIn({ login: 'abc', password: 'abc' });
     } else {
-      signOut();
+      await signOut();
     }
 
     onSubmit();
   };
 
   return (
-    <div className="fixed top-1/4 left-1/2 transform -translate-x-1/2 w-11/12 bg-grey-800 rounded-md z-50">
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        onSubmit={handleSubmit}
+    <Backdrop onClick={onClose}>
+      <div
+        className="fixed top-1/4 left-1/2 transform -translate-x-1/2 w-11/12 bg-grey-800 rounded-md z-50"
+        role="presentation"
+        onClick={e => e.stopPropagation()}
       >
-        <Form className="flex flex-col gap-3 p-4">
-          <FieldLabel label="Email" htmlFor="email">
-            <Input formik type="email" name="email" placeholder="Email" />
-          </FieldLabel>
+        <Button buttonStyle="tertiary" onClick={() => populateJSON()}>
+          Populate database
+        </Button>
 
-          <FieldLabel label="Password" htmlFor="password">
-            <Input
-              formik
-              type="password"
-              name="password"
-              placeholder="Password"
-            />
-          </FieldLabel>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={handleSubmit}
+        >
+          <Form className="flex flex-col gap-3 p-4">
+            <FieldLabel label="Email" htmlFor="email">
+              <Input formik type="email" name="email" placeholder="Email" />
+            </FieldLabel>
 
-          <div className="flex gap-1">
-            <span className="text-gray-200">Forgot your password?</span>
+            <FieldLabel label="Password" htmlFor="password">
+              <Input
+                formik
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+            </FieldLabel>
 
-            <span className="text-movieHouse-mid cursor-pointer">
-              Reset here
-            </span>
-          </div>
+            <div className="flex gap-1">
+              <span className="text-gray-200">Forgot your password?</span>
 
-          <div className="flex gap-1">
-            <span className="text-gray-200">Dont have an account?</span>
+              <span className="text-movieHouse-mid cursor-pointer">
+                Reset here
+              </span>
+            </div>
 
-            <span className="text-movieHouse-mid cursor-pointer">
-              Register now
-            </span>
-          </div>
+            <div className="flex gap-1">
+              <span className="text-gray-200">Dont have an account?</span>
 
-          <Button type="submit">Submit</Button>
-        </Form>
-      </Formik>
-    </div>
+              <span className="text-movieHouse-mid cursor-pointer">
+                Register now
+              </span>
+            </div>
+
+            <Button type="submit">Submit</Button>
+          </Form>
+        </Formik>
+      </div>
+    </Backdrop>
   );
 };
 
