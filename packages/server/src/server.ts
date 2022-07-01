c3338908db1f8bd767847dd43fe69f1f4640d7f9
Victor <vhlima2000@gmail.com>
2022-07-01 05:51:09 -0300

    ✨ Adds database connection and user resolver

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,27 +1,30 @@
-import express, { Request, Response } from 'express';
+import 'reflect-metadata';
 
-const SERVER_PORT = 3333;
+import path from 'path';
 
-const app = express();
+import { ApolloServer } from 'apollo-server';
 
-// app.use(cors());
-app.use(express.json());
-// app.use('/files', express.static(uploadConfig.directory));
-// app.use(routes);
+import { buildSchema } from 'type-graphql';
 
-app.use((err: Error, req: Request, res: Response) => {
-  console.error(err);
+import UserResolver from './resolvers/UserResolver';
 
-  return res.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
+import database from './database';
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [UserResolver],
+    emitSchemaFile: path.resolve(__dirname, 'schema.graphql'),
   });
-});
 
-app.get('/', (err: Error, req: Request, res: Response) =>
-  res.status(200).json({ message: 'Sucess!' }),
-);
+  const server = new ApolloServer({
+    schema,
+  });
 
-app.listen(SERVER_PORT, () => {
-  console.log(`[Movie House] Server started on port: ${SERVER_PORT}`);
-});
+  const { url } = await server.listen();
+
+  database();
+
+  console.log(`[Movie House] Server running on ${url}`);
+};
+
+main();
