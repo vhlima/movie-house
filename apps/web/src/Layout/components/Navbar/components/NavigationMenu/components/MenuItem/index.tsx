befe41f5b4b9f9216da9431722063a716a31ca0d
Victor <vhlima2000@gmail.com>
2022-07-06 03:55:25 -0300

    ✨ Adds menu item component, substitute for menu link

---
+++ apps/web/src/Layout/components/Navbar/components/NavigationMenu/components/MenuItem/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import Link, { LinkProps } from '../../../../../../../components/Link';
+
+import SvgIcon, { SvgIconType } from '../../../../../../../components/SvgIcon';
+
+interface MenuItemButtonProps {
+  onClick: () => void;
+}
+
+interface MenuLinkProps {
+  text: string;
+  icon: SvgIconType;
+  itemProps: LinkProps | MenuItemButtonProps;
+}
+
+const MenuLink: React.FC<MenuLinkProps> = ({ text, icon, itemProps }) => {
+  const ItemBody = (
+    <div className="flex items-center gap-3">
+      <SvgIcon className="text-grey-300" iconType={icon} size={22} />
+
+      <span className="text-grey-200 uppercase font-semibold group-hover:text-grey-300">
+        {text}
+      </span>
+    </div>
+  );
+
+  const itemPropsAsLink = itemProps as LinkProps;
+
+  return (
+    <li className="border-b border-b-grey-300 group last:border-0 py-2 first:pt-0 last:pb-0">
+      {!itemPropsAsLink.href ? (
+        <button
+          className="flex items-center w-full"
+          type="button"
+          onClick={(itemProps as MenuItemButtonProps).onClick}
+        >
+          {ItemBody}
+        </button>
+      ) : (
+        <Link className="block" {...itemPropsAsLink}>
+          {ItemBody}
+        </Link>
+      )}
+    </li>
+  );
+};
+
+export default MenuLink;
