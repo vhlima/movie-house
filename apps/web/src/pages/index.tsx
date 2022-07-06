02c011c98156c2e91e9e76fe84d8620d8b3840fa
Victor <vhlima2000@gmail.com>
2022-07-06 06:11:25 -0300

    ✨ Adds feature links to home page

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,32 +1,97 @@
 import type { NextPage } from 'next';
 
-import { MovieData } from '../types';
+import Card from '../components/Card';
 
-import * as fakeData from '../data/fake.json';
+import Logo from '../components/Logo';
+
+import FeatureLink from '../views/home/FeatureLink';
+
+import BackgroundImage from '../components/BackgroundImage';
 
 import Layout from '../Layout';
 
-import Card from '../components/Card';
-
-import MovieCarousel from '../components/MovieCarousel';
-import UserMovieReview from '../components/UserMovieReview';
-
 const Home: NextPage = () => (
   <Layout>
-    <Card
-      link={{ href: '/what-to-watch/fan-favorites' }}
-      title="Top choices"
-      description="Movies we think you might like"
-    >
-      <MovieCarousel movies={fakeData.movies as MovieData[]} />
-    </Card>
+    <div className="flex">
+      <BackgroundImage src="https://a.ltrbxd.com/resized/sm/upload/ah/66/ey/4h/top-gun-maverick-1200-1200-675-675-crop-000000.jpg" />
 
-    <Card title="Popular reviews this week">
-      <UserMovieReview preview />
-      <UserMovieReview preview />
-      <UserMovieReview preview />
-      <UserMovieReview preview />
-    </Card>
+      <div className="w-full mt-32 z-10 p-3">
+        <div className="flex flex-col items-center gap-2 mb-16">
+          <Logo logoSize="lg" showLogo={false} showText />
+
+          <p className="text-grey-200 text-xl text-center">
+            Track films youve watched. <br />
+            Save those you want to see. <br />
+            Tell your friends whats good.
+          </p>
+
+          <span className="text-grey-300">
+            The social network for movie lovers.
+          </span>
+        </div>
+
+        <div>
+          <Card title="Features you will love" noPadding>
+            <FeatureLink
+              href="/"
+              iconType="IoEye"
+              color="green"
+              text="Keep track of every film youve ever watched (or just start from the day you join)"
+            />
+
+            <FeatureLink
+              href="/"
+              iconType="AiFillHeart"
+              color="orange"
+              text="Show some love for your favorite movies, lists and reviews by liking, commenting and reviewing"
+            />
+
+            <FeatureLink
+              href="/"
+              iconType="IoList"
+              color="purple"
+              text="Write and share reviews, and follow friends and other members to read theirs"
+            />
+
+            <FeatureLink
+              href="/"
+              iconType="AiFillStar"
+              color="green"
+              text="Rate each film on a ten-star scale (with halves) to record and share your reaction"
+            />
+
+            <FeatureLink
+              href="/"
+              iconType="AiOutlineCalendar"
+              color="orange"
+              text="Keep a diary of your film watching"
+            />
+
+            <FeatureLink
+              href="/"
+              iconType="AiFillHeart"
+              color="purple"
+              text="Compile and share lists of films on any topic and keep a watchlist of films to see"
+            />
+          </Card>
+        </div>
+
+        {/* <Card
+          title="Top choices"
+          description="Movies we think you might like"
+          link={{ href: '/what-to-watch/fan-favorites' }}
+        >
+          <MovieCarousel movies={fakeData.movies as MovieData[]} />
+        </Card>
+
+        <Card title="Popular reviews this week">
+          <UserMovieReview preview />
+          <UserMovieReview preview />
+          <UserMovieReview preview />
+          <UserMovieReview preview />
+        </Card> */}
+      </div>
+    </div>
   </Layout>
 );
 
---
+++ apps/web/src/views/home/FeatureLink/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import clsx from 'clsx';
+
+import Link, { LinkProps } from '../../../components/Link';
+
+import SvgIcon, { SvgIconType } from '../../../components/SvgIcon';
+
+interface FeatureLinkProps extends LinkProps {
+  iconType: SvgIconType;
+  text: string;
+  color: 'purple' | 'orange' | 'green';
+}
+
+const FeatureLink: React.FC<FeatureLinkProps> = ({
+  iconType,
+  text,
+  color,
+  ...linkProps
+}) => (
+  <Link
+    className={clsx(
+      'flex gap-2 p-4 rounded-md border-2 border-grey-600 group hover:border-transparent',
+      {
+        'hover:bg-success-base': color === 'green',
+        'hover:bg-warning-base': color === 'orange',
+        'hover:bg-movieHouse-mid': color === 'purple',
+      },
+    )}
+    {...linkProps}
+  >
+    <SvgIcon
+      className={clsx('flex-shrink-0 text-grey-500 group-hover:text-grey-200')}
+      iconType={iconType}
+      size={50}
+    />
+
+    <p className="text-grey-100 group-hover:text-white">{text}</p>
+  </Link>
+);
+
+export default FeatureLink;
