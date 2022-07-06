aecf615376c4a3d4753f020c3283796b86207ce4
Victor <vhlima2000@gmail.com>
2022-07-06 03:46:13 -0300

    ✨ Modals can have auto backdrop

--- apps/web/src/components/Modal/index.tsx
+++ apps/web/src/components/Modal/index.tsx
@@ -1,18 +1,52 @@
-import React, { PropsWithChildren, Children } from 'react';
+import React, { PropsWithChildren } from 'react';
+
+import clsx from 'clsx';
 
 import { createPortal } from 'react-dom';
 
+import { MotionProps, motion } from 'framer-motion';
+
+import Backdrop from '../Backdrop';
+
 interface ModalProps {
-  portalId: string;
+  className?: string;
+  portalId?: string;
+  animation?: MotionProps;
+  onClickBackdrop?: () => void;
 }
 
+const PORTAL_ID = 'modalPortal';
+
 const Modal: React.FC<PropsWithChildren<ModalProps>> = ({
-  portalId,
+  className,
+  animation,
+  onClickBackdrop,
   children,
-}) =>
-  createPortal(
-    Children.only(children),
-    document.getElementById(portalId) || document.body,
+}) => {
+  const body = (
+    <motion.div
+      className={clsx('p-4 bg-grey-800 z-50', className)}
+      role="presentation"
+      onClick={e => e.stopPropagation()}
+      {...animation}
+    >
+      {children}
+    </motion.div>
   );
+
+  const modal = !onClickBackdrop ? (
+    body
+  ) : (
+    <Backdrop onClick={onClickBackdrop}>{body}</Backdrop>
+  );
+
+  // fixed top-1/4 left-1/2 transform -translate-x-1/2 w-11/12 rounded-md
+  // absolute bottom-0 w-full rounded-t-md z-50
+
+  return createPortal(
+    modal,
+    document.getElementById(PORTAL_ID) || document.body,
+  );
+};
 
 export default Modal;
