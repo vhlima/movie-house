2c7da81bc792355153c4ca4cb7b5d17bfd1a69c4
Victor <vhlima2000@gmail.com>
2022-07-08 06:19:14 -0300

    🎨 Automatically append images url

--- apps/server/src/entities/movie/movie.interface.ts
+++ apps/server/src/entities/movie/movie.interface.ts
@@ -1,4 +1,4 @@
-import { Field, Float, ObjectType } from 'type-graphql';
+import { Field, Float, ObjectType, Root } from 'type-graphql';
 
 import { prop } from '@typegoose/typegoose';
 
@@ -34,7 +34,21 @@ export default class Movie {
   @prop()
   readonly poster_path: string;
 
+  @Field()
+  @prop()
+  readonly backdrop_path: string;
+
   @Field(() => Date)
   @prop({ type: () => Date })
   readonly release_date: Date;
+
+  @Field()
+  posterUrl(@Root() movie: Movie): string {
+    return `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  }
+
+  @Field()
+  backdropUrl(@Root() movie: Movie): string {
+    return `https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`;
+  }
 }
