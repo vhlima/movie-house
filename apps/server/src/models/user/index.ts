c947989da05b61a6b473f9eab6374a6308c369bf
Victor <vhlima2000@gmail.com>
2022-07-08 05:06:36 -0300

    🔥 All MC structure is now using entities folder

--- apps/server/src/models/user/index.ts
+++
@@ -1,9 +0,0 @@
-import { getModelForClass } from '@typegoose/typegoose';
-
-import User from './User';
-
-import Review from './Review';
-
-export const UserModel = getModelForClass(User);
-
-export const ReviewModel = getModelForClass(Review);
--- apps/server/src/resolvers/user/index.ts
+++
@@ -1,7 +0,0 @@
-import UserResolver from './UserResolver';
-
-import ReviewResolver from './ReviewResolver';
-
-export const userResolver = UserResolver;
-
-export const reviewResolver = ReviewResolver;
