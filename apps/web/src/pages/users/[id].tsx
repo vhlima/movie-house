97e3bc5ccb065fb7a69790724337b95ea1f00a55
Victor <vhlima2000@gmail.com>
2022-07-11 04:37:01 -0300

    🎨 Improve graphql queries to fetch user profile

--- apps/web/src/pages/users/[id].tsx
+++ apps/web/src/pages/users/[id].tsx
@@ -2,7 +2,7 @@ import type { GetStaticPaths, NextPage } from 'next';
 
 import { GetStaticProps } from 'next';
 
-import { UserData } from '../../types';
+import type { UserResponse } from '../../types/user';
 
 import { USER, ALL_USERS_ID } from '../../graphql/user';
 
@@ -17,31 +17,30 @@ import ProfileHeader from '../../views/users/ProfileHeader';
 import FavoriteMovies from '../../views/users/FavoriteMovies';
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  if (params) {
-    const { id } = params;
+  if (!params || !params.id) return { props: { targetUser: undefined } };
 
-    if (id) {
-      try {
-        const usersResult = await client.query<{ users: UserData[] }>({
-          query: USER,
-        });
+  const { id } = params;
 
-        return {
-          props: {
-            targetUser: usersResult.data.users.find(u => u._id === id),
-          },
-        };
-      } catch (err) {
-        console.error(err);
-      }
-    }
+  try {
+    const userResult = await client.query<{ user: UserResponse }>({
+      query: USER,
+      variables: { userId: id },
+    });
+
+    return {
+      props: {
+        targetUser: userResult.data.user,
+      },
+    };
+  } catch (err) {
+    console.error(err);
   }
 
   return { props: { targetUser: undefined } };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const usersResult = await client.query<{ users: UserData[] }>({
+  const usersResult = await client.query<{ users: UserResponse[] }>({
     query: ALL_USERS_ID,
   });
 
@@ -53,7 +52,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-const UserProfile: NextPage<{ targetUser: UserData }> = ({ targetUser }) => {
+const UserProfile: NextPage<{ targetUser: UserResponse }> = ({
+  targetUser,
+}) => {
   if (!targetUser) {
     return <h1 className="text-red-500">User not found</h1>;
   }
