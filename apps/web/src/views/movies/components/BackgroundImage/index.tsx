a80068a5580fb25667e4d1f868e786117e16a241
Victor <vhlima2000@gmail.com>
2022-07-11 04:28:14 -0300

    💄 Smoother background gradient

--- apps/web/src/views/movies/components/BackgroundImage/index.tsx
+++ apps/web/src/views/movies/components/BackgroundImage/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-
 import Image, { ImageProps } from 'next/image';
 
 const BackgroundImage: React.FC<ImageProps> = ({ ...imageProps }) => (
   <div className="w-full h-48 absolute z-0">
     <Image layout="fill" objectFit="fill" {...imageProps} />
 
-    <div className="absolute bottom-0 h-20 w-full z-10 bg-gradient-to-t from-grey-900" />
+    <div
+      className="relative h-full"
+      style={{
+        imageRendering: 'pixelated',
+        backgroundImage:
+          'linear-gradient(to bottom, transparent, rgba(37, 37, 37, 0.61), #111)',
+      }}
+    />
   </div>
 );
 
