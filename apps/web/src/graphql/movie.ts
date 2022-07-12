4ea7f993f787e877d73be54bb891d40503501f26
Victor <vhlima2000@gmail.com>
2022-07-12 07:03:58 -0300

    🎨 Adds movie fields to abstract field returning

--- apps/web/src/graphql/movie.ts
+++ apps/web/src/graphql/movie.ts
@@ -1,33 +1,41 @@
 import { gql } from '@apollo/client';
 
+export const MOVIE_FIELDS = gql`
+  fragment MovieFields on Movie {
+    id
+    imdb_id
+    original_language
+    original_title
+    overview
+    runtime
+    vote_average
+    poster_path
+    backdrop_path
+    release_date
+    posterUrl
+    backdropUrl
+    genres {
+      id
+      name
+    }
+    production_companies {
+      id
+      name
+      logo_path
+    }
+    spoken_languages {
+      english_name
+      iso_639_1
+    }
+  }
+`;
+
 export const GET_MOVIE = gql`
+  ${MOVIE_FIELDS}
+
   query GetMovie($movieId: String!) {
     getMovie(movieId: $movieId) {
-      id
-      imdb_id
-      original_language
-      original_title
-      overview
-      runtime
-      vote_average
-      poster_path
-      backdrop_path
-      release_date
-      posterUrl
-      backdropUrl
-      genres {
-        id
-        name
-      }
-      production_companies {
-        id
-        name
-        logo_path
-      }
-      spoken_languages {
-        english_name
-        iso_639_1
-      }
+      ...MovieFields
     }
   }
 `;
