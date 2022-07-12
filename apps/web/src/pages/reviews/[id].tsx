71bd53cf491f0c3d7ecbb19594b7da82c47b62cd
Victor <vhlima2000@gmail.com>
2022-07-12 07:05:05 -0300

    ✨ Review being returned by next ssg

--- apps/web/src/pages/reviews/[id].tsx
+++ apps/web/src/pages/reviews/[id].tsx
@@ -1,19 +1,55 @@
-import { NextPage } from 'next';
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 
-import { MovieData } from '../../types';
+import type { MovieData } from '../../types';
 
-import * as fakeData from '../../data/fake.json';
+import type { ReviewResponse } from '../../types/user';
 
-import MovieHeader from '../../views/movies/Header';
+import { GET_REVIEW } from '../../graphql/user';
+
+import client from '../../api';
+
+import MovieHeader from '../../views/movies/view/Header';
 
 import UserMovieReview from '../../views/users/reviews';
 
-const Reviews: NextPage = () => {
-  const movie = fakeData.movies[0] as MovieData;
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const defaultProps = { props: { movie: undefined } };
+
+  const { id } = params;
+
+  if (!id) return defaultProps;
+
+  try {
+    const reviewResponse = await client.query<{ getReview: ReviewResponse }>({
+      query: GET_REVIEW,
+      variables: { reviewId: id },
+    });
+
+    return {
+      props: {
+        review: reviewResponse.data.getReview,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+  }
+
+  return defaultProps;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: [],
+  fallback: true,
+});
+
+const Reviews: NextPage<{ review: ReviewResponse }> = ({ review }) => {
+  if (!review) {
+    return <h1>Review not found</h1>;
+  }
 
   return (
-    <MovieHeader movie={movie}>
-      <UserMovieReview />
+    <MovieHeader movie={review.movie}>
+      <UserMovieReview review={review} />
     </MovieHeader>
   );
 };
