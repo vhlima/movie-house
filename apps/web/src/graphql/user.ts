456bab9da7d4c82b7bb8fe514f993979fbd9fa27
Victor <vhlima2000@gmail.com>
2022-07-12 07:04:07 -0300

    🎨 Adds user fields to abstract field returning

--- apps/web/src/graphql/user.ts
+++ apps/web/src/graphql/user.ts
@@ -1,48 +1,63 @@
 import { gql } from '@apollo/client';
 
-export const USER = gql`
-  query ($userId: String!) {
-    user(UserId: $userId) {
+import type { DocumentNode } from '@apollo/client';
+
+import { MOVIE_FIELDS } from './movie';
+
+const UserFields = gql`
+  ${MOVIE_FIELDS}
+
+  fragment UserFields on User {
+    _id
+    username
+    realName
+    profilePicture
+    favoriteMovies {
+      id
+      original_title
+      posterUrl
+    }
+    moviesInfo {
+      watched
+      rating
+      liked
+      review {
+        body
+      }
+      movie {
+        ...MovieFields
+      }
+    }
+    followers {
       _id
-      username
-      realName
-      profilePicture
-      favoriteMovies {
-        id
-        original_title
-        posterUrl
-      }
-      followers {
-        _id
-      }
-      following {
-        _id
-      }
+    }
+    following {
+      _id
     }
   }
 `;
 
-export const ALL_USERS = gql`
-  query users {
-    users {
-      _id
-      username
-      realName
-      profilePicture
-      favoriteMovies {
-        id
-        original_title
-        posterUrl
-      }
-      followers {
-        _id
-      }
-      following {
-        _id
-      }
+const userFieldsFragmentBuilder = (doc: DocumentNode) => gql`
+  ${UserFields}
+
+  ${doc}
+`;
+
+export const USER = userFieldsFragmentBuilder(gql`
+  query ($userId: String!) {
+    user(UserId: $userId) {
+      ...UserFields
     }
   }
-`;
+`);
+
+export const ALL_USERS = userFieldsFragmentBuilder(gql`
+  query users {
+    users {
+      ...UserFields
+    }
+  }
+`);
 
 export const ALL_USERS_ID = gql`
   query users {
@@ -52,68 +67,57 @@ export const ALL_USERS_ID = gql`
   }
 `;
 
-export const SIGN_IN = gql`
+export const SIGN_IN = userFieldsFragmentBuilder(gql`
   mutation ($username: String!) {
     userLogin(username: $username) {
-      _id
-      username
-      realName
-      profilePicture
-      favoriteMovies {
-        id
-        original_title
-        posterUrl
-      }
-      followers {
-        _id
-      }
-      following {
-        _id
-      }
+      ...UserFields
     }
   }
-`;
+`);
 
-export const ADD_FAVORITE_MOVIE = gql`
+export const ADD_FAVORITE_MOVIE = userFieldsFragmentBuilder(gql`
   mutation ($movieId: String!, $userId: String!) {
     userAddFavoriteMovie(movieId: $movieId, userId: $userId) {
+      ...UserFields
+    }
+  }
+`);
+
+export const REMOVE_FAVORITE_MOVIE = userFieldsFragmentBuilder(gql`
+  mutation ($movieId: String!, $userId: String!) {
+    userRemoveFavoriteMovie(movieId: $movieId, userId: $userId) {
+      ...UserFields
+    }
+  }
+`);
+
+export const CREATE_REVIEW = gql`
+  mutation ($body: String!, $movieId: String!, $userId: String!) {
+    createReview(body: $body, movieId: $movieId, userId: $userId) {
       _id
-      username
-      realName
-      profilePicture
-      favoriteMovies {
-        id
+    }
+  }
+`;
+
+export const GET_REVIEW = gql`
+  query ($reviewId: String!) {
+    getReview(reviewId: $reviewId) {
+      _id
+      body
+      movie {
         original_title
+        release_date
+        backdropUrl
         posterUrl
-      }
-      followers {
-        _id
-      }
-      following {
-        _id
       }
     }
   }
 `;
 
-export const REMOVE_FAVORITE_MOVIE = gql`
-  mutation ($movieId: String!, $userId: String!) {
-    userRemoveFavoriteMovie(movieId: $movieId, userId: $userId) {
-      _id
-      username
-      realName
-      profilePicture
-      favoriteMovies {
-        id
-        original_title
-        posterUrl
-      }
-      followers {
-        _id
-      }
-      following {
-        _id
-      }
+export const ADD_MOVIE_INFO = userFieldsFragmentBuilder(gql`
+  mutation ($data: MovieInfoInput!) {
+    userAddMovieInfo(data: $data) {
+      ...UserFields
     }
   }
-`;
+`);
