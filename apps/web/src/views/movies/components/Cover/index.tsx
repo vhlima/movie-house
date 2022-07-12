8705b12bdbb11bc5ee43d548de981f22c5026174
Victor <vhlima2000@gmail.com>
2022-07-12 01:35:27 -0300

    🎨 Movie cover now supports all three types of covers

--- apps/web/src/views/movies/components/Cover/index.tsx
+++ apps/web/src/views/movies/components/Cover/index.tsx
@@ -1,29 +1,70 @@
-import React from 'react';
+import type { PropsWithChildren } from 'react';
 
 import Image from 'next/image';
 
 import clsx from 'clsx';
 
 interface MovieCoverProps {
-  coverUrl: string;
+  coverUrl?: string;
+
+  coverStyle?: 'primary' | 'secondary';
   coverSize?: 'lg' | 'md' | 'sm' | 'full';
+
+  onClick?: () => void;
 }
 
-const MovieCover: React.FC<MovieCoverProps> = ({
+// const emptyCardStyle = clsx(
+//   'flex items-center justify-center flex-shrink-0 w-full h-28 rounded-md border-2 text-grey-500 select-none',
+//   {
+//     'border-grey-800': cardStyle === 'primary',
+//     'border-grey-500': cardStyle === 'secondary',
+//     'w-full': cardSize === 'full',
+//     'w-20 h-28': cardSize === 'sm',
+//   },
+// );
+
+const MovieCover: React.FC<PropsWithChildren<MovieCoverProps>> = ({
+  coverStyle = 'primary',
   coverSize = 'sm',
   coverUrl,
-}) => (
-  <div
-    className={clsx(
-      'relative rounded-lg border border-grey-700 overflow-hidden flex-shrink-0',
-      {
-        'w-20 h-28': coverSize === 'sm',
-        'w-full h-full': coverSize === 'full',
-      },
-    )}
-  >
-    <Image layout="fill" objectFit="fill" src={coverUrl} />
-  </div>
-);
+  onClick,
+  children,
+}) => {
+  const coverContainerStyle = clsx(
+    'relative rounded-lg border border-grey-700 overflow-hidden flex-shrink-0',
+    {
+      'flex items-center justify-center text-grey-500 select-none': !coverUrl,
+
+      'w-20 h-28': coverSize === 'sm',
+      'w-full h-28': coverSize === 'full',
+
+      'border-grey-800': coverStyle === 'primary',
+      'border-grey-500': coverStyle === 'secondary',
+    },
+  );
+
+  return !onClick ? (
+    <div className={coverContainerStyle}>
+      {coverUrl ? (
+        <Image layout="fill" objectFit="fill" src={coverUrl} />
+      ) : (
+        <span className="text-3xl">?</span>
+      )}
+    </div>
+  ) : (
+    <button
+      className={clsx(
+        'outline-none hover:border-movieHouse-mid focus:border-movieHouse-mid',
+        coverContainerStyle,
+      )}
+      type="button"
+      onClick={onClick}
+    >
+      <div className="flex items-center justify-center w-full p-2">
+        {children}
+      </div>
+    </button>
+  );
+};
 
 export default MovieCover;
