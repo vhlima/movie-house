616af04b66676f89c3063e08c49c2c6b6a95b3a0
Victor <vhlima2000@gmail.com>
2022-07-14 07:41:52 -0300

    ✨ Adds gqlAppend function

--- apps/web/src/utils/index.ts
+++ apps/web/src/utils/index.ts
@@ -1,5 +1,10 @@
 import { DocumentNode, gql } from '@apollo/client';
 
+// TODO change that
+
 export const appendGql = (...docs: DocumentNode[]) => gql`
-  ${docs.map(doc => doc)}
-`;
+    # ${docs.map(doc => `${doc}`)}
+
+    ${docs[0]}
+    ${docs[1]}
+  `;
