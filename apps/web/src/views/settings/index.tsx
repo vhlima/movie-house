839004d703bc6e64d4a01d8ab8630b5b4c58cc25
Victor <vhlima2000@gmail.com>
2022-07-14 07:42:32 -0300

    🚚 Move settings page content to views

---
+++ apps/web/src/views/settings/index.tsx
@@ -0,0 +1,118 @@
+import { useRef } from 'react';
+
+import { Form, Formik } from 'formik';
+
+import { useAuth } from '../../hooks/useAuth';
+
+import { useLogic } from './logic';
+
+import Card from '../../components/Card';
+
+import Input from '../../components/Input';
+
+import Button from '../../components/Button';
+
+import UserProfilePicture from '../users/components/ProfilePicture';
+
+const ProfileSettingsView: React.FC = () => {
+  const { user } = useAuth();
+
+  const {
+    updateUserResult,
+
+    uploadedProfilePictureUrl,
+    clearUploadedProfilePictureUrl,
+
+    handleProfilePictureAdd,
+
+    handleSubmit,
+  } = useLogic();
+
+  const profilePictureInputRef = useRef<HTMLInputElement | null>(null);
+
+  if (!user) {
+    return <h1 className="text-danger-base">Must be logged</h1>;
+  }
+
+  return (
+    <Card title="Personal settings">
+      <Formik
+        initialValues={{
+          username: user.username,
+          realName: user.realName,
+          biography: user.biography,
+          email: user.email,
+        }}
+        onSubmit={handleSubmit}
+      >
+        <Form className="flex flex-col gap-2">
+          <div className="flex items-center gap-2">
+            <UserProfilePicture
+              imageSize="lg"
+              src={uploadedProfilePictureUrl || user.profilePicture}
+            />
+
+            <input
+              className="hidden"
+              type="file"
+              accept="image/*"
+              name="profilePictureFile"
+              ref={profilePictureInputRef}
+              onChange={handleProfilePictureAdd}
+            />
+
+            <div className="flex flex-col gap-2 w-full">
+              <Button
+                buttonStyle="secondary"
+                onClick={() => profilePictureInputRef?.current.click()}
+              >
+                Upload profile picture
+              </Button>
+
+              {uploadedProfilePictureUrl && (
+                <Button
+                  buttonStyle="danger"
+                  onClick={clearUploadedProfilePictureUrl}
+                >
+                  Remove uploaded picture
+                </Button>
+              )}
+            </div>
+          </div>
+
+          <Input
+            formik
+            name="username"
+            label={{ text: 'Username', htmlFor: true }}
+          />
+
+          <Input
+            formik
+            name="realName"
+            label={{ text: 'Real name', htmlFor: true }}
+          />
+
+          <Input
+            formik
+            type="email"
+            name="email"
+            label={{ text: 'Email', htmlFor: true }}
+          />
+
+          <Input
+            formik
+            textarea
+            name="biography"
+            label={{ text: 'Bio', htmlFor: true }}
+          />
+
+          <Button type="submit" disabled={updateUserResult.loading}>
+            Save changes
+          </Button>
+        </Form>
+      </Formik>
+    </Card>
+  );
+};
+
+export default ProfileSettingsView;
---
+++ apps/web/src/views/settings/logic.ts
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+
+import type { MutationResult } from '@apollo/client';
+
+import type { ChangeEvent } from 'react';
+
+import type { UserResponse } from '../../types/user';
+
+import { useAuth } from '../../hooks/useAuth';
+
+import { UPDATE_USER } from '../../graphql/user';
+
+interface ProfileSettingsProps {
+  username: string;
+  realName: string;
+  biography: string;
+}
+
+type SubmitHandles = (values: ProfileSettingsProps) => Promise<void>;
+
+type AddProfilePictureHandles = (e: ChangeEvent<HTMLInputElement>) => void;
+
+type UpdateUserResponse = { userUpdate: UserResponse };
+
+interface ProfileSettingsLogicHandles {
+  updateUserResult: MutationResult<UpdateUserResponse>;
+
+  uploadedProfilePictureUrl: string;
+  clearUploadedProfilePictureUrl: () => void;
+
+  handleProfilePictureAdd: AddProfilePictureHandles;
+
+  handleSubmit: SubmitHandles;
+}
+
+export const useLogic = (): ProfileSettingsLogicHandles => {
+  const { user, setUser } = useAuth();
+
+  const [updateUserMutation, updateUserResult] =
+    useMutation<UpdateUserResponse>(UPDATE_USER);
+
+  const [uploadedProfilePictureUrl, setUploadedProfilePictureUrl] =
+    useState<string>('');
+
+  const clearUploadedProfilePictureUrl = () => {
+    setUploadedProfilePictureUrl('');
+  };
+
+  // TODO error handling
+
+  const handleSubmit: SubmitHandles = async values => {
+    if (!user) return;
+
+    const { data } = await updateUserMutation({
+      variables: { userId: user._id, data: values },
+    });
+
+    if (data) {
+      setUser(data.userUpdate);
+    }
+  };
+
+  const handleProfilePictureAdd: AddProfilePictureHandles = e => {
+    const { files } = e.currentTarget;
+
+    if (files) {
+      // setFieldValue('image_files', files);
+      setUploadedProfilePictureUrl(URL.createObjectURL(files[0]));
+    }
+  };
+
+  return {
+    updateUserResult,
+
+    uploadedProfilePictureUrl,
+    clearUploadedProfilePictureUrl,
+
+    handleProfilePictureAdd,
+
+    handleSubmit,
+  };
+};
