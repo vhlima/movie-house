a17398b1480c451a1d62119738d834ea86334abe
Victor <vhlima2000@gmail.com>
2022-07-14 07:38:14 -0300

    🔥 Remove unused function

--- apps/server/src/controllers/user.controller.ts
+++ apps/server/src/controllers/user.controller.ts
@@ -1,9 +1,5 @@
 import type { DocumentType } from '@typegoose/typegoose';
 
-import type { DatasourceContext } from '../api';
-
-import Movie from '../entities/movie.interface';
-
 import User from '../entities/user.interface';
 
 import { UserModel } from '../models';
@@ -18,25 +14,4 @@ export const findUserById = async (
   }
 
   return user;
-};
-
-// TODO maybe remove that
-export const findUserAndMovie = async (
-  context: DatasourceContext,
-  userId: string,
-  movieId: string,
-): Promise<[DocumentType<User>, DocumentType<Movie>]> => {
-  const user = await UserModel.findById(userId);
-
-  if (!user) {
-    throw new Error('User not found');
-  }
-
-  const movie = await context.dataSources.tmdb.getMovieById(movieId);
-
-  if (!movie) {
-    throw new Error('Movie not found');
-  }
-
-  return [user, movie];
 };
