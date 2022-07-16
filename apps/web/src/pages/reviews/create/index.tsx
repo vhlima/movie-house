95225b59cbd0211f16c0c7ae8d7d4591c9c88865
Victor <vhlima2000@gmail.com>
2022-07-16 06:40:20 -0300

    🏗️ Move review create page to views

--- apps/web/src/pages/reviews/create/index.tsx
+++ apps/web/src/pages/reviews/create/index.tsx
@@ -1,121 +1,50 @@
-import { useState } from 'react';
-
-import { Formik, Form } from 'formik';
-
-import { useMutation } from '@apollo/client';
-
-import { useRouter } from 'next/router';
-
-import type { NextPage } from 'next';
-
-import type { ReviewData, ReviewResponse } from '../../../types/user';
+import type { GetServerSideProps, NextPage } from 'next';
 
 import type { MovieResponse } from '../../../types/movie';
 
-import { useAuth } from '../../../hooks/useAuth';
+import { GET_MOVIE } from '../../../graphql/movie';
 
-import { CREATE_REVIEW } from '../../../graphql/review';
+import CreateReviewView from '../../../views/reviews/create';
 
-import Card from '../../../components/Card';
+import client from '../../../api';
 
-import Input from '../../../components/Input';
+export interface CreateReviewPageProps {
+  movie?: MovieResponse;
+}
 
-import Button from '../../../components/Button';
+export const getServerSideProps: GetServerSideProps = async context => {
+  const defaultProps = { props: { movie: null } };
 
-import MovieInfo from '../../../views/movies/components/Info';
+  const { query } = context;
 
-import MovieCover from '../../../views/movies/components/Cover';
+  if (!query) return defaultProps;
 
-import MovieSearchModal from '../../../views/movies/components/SearchModal';
+  const { movie: movieId } = query;
 
-const CreateReviewPage: NextPage = () => {
-  const { user } = useAuth();
+  if (!movieId || typeof movieId !== 'string') return defaultProps;
 
-  const { push } = useRouter();
-
-  const [createReviewMutation] = useMutation<{ createReview: ReviewResponse }>(
-    CREATE_REVIEW,
-  );
-
-  const [selectedMovie, setSelectedMovie] = useState<MovieResponse>();
-
-  const [isSearch, setSearch] = useState<boolean>(false);
-
-  const handleSubmit = async (values: ReviewData) => {
-    if (!user) return;
-
-    const reviewResponse = await createReviewMutation({
-      variables: {
-        userId: user._id,
-        movieId: selectedMovie.id,
-        data: values,
-      },
+  try {
+    const { data } = await client.query<{ movie: MovieResponse }>({
+      query: GET_MOVIE,
+      variables: { movieId },
     });
 
-    if (!reviewResponse.data) return;
+    if (data) {
+      return {
+        props: {
+          movie: data.movie,
+        } as CreateReviewPageProps,
+      };
+    }
+  } catch (err) {
+    console.error(err);
+  }
 
-    push({
-      pathname: '/reviews/[id]',
-      query: { id: reviewResponse.data.createReview._id },
-    });
-  };
-
-  const handleSelect = (movie: MovieResponse) => {
-    setSelectedMovie(movie);
-    setSearch(false);
-  };
-
-  return (
-    <>
-      {isSearch && (
-        <MovieSearchModal
-          title="Search a movie to review"
-          onSelect={handleSelect}
-          onClose={() => setSearch(false)}
-        />
-      )}
-
-      <Card title="Write you review about a movie">
-        <div className="flex flex-col gap-2">
-          <div className="flex gap-2">
-            {!selectedMovie ? (
-              <>
-                <MovieCover coverSize="sm" />
-
-                <div>
-                  <h1 className="text-grey-100 text-lg whitespace-nowrap">
-                    No movie selected
-                  </h1>
-
-                  <p className="text-grey-200">
-                    Select the movie you want to review
-                  </p>
-                </div>
-              </>
-            ) : (
-              <MovieInfo movie={selectedMovie} />
-            )}
-          </div>
-
-          <Button buttonStyle="secondary" onClick={() => setSearch(true)}>
-            Select movie
-          </Button>
-        </div>
-
-        <Formik initialValues={{ body: '' }} onSubmit={handleSubmit}>
-          <Form className="flex flex-col gap-2">
-            <Input
-              textarea
-              name="body"
-              label={{ text: 'Your review:', htmlFor: true }}
-            />
-
-            <Button type="submit">Post review</Button>
-          </Form>
-        </Formik>
-      </Card>
-    </>
-  );
+  return defaultProps;
 };
+
+const CreateReviewPage: NextPage<CreateReviewPageProps> = ({ movie }) => (
+  <CreateReviewView movie={movie} />
+);
 
 export default CreateReviewPage;
--- apps/web/src/views/users/reviews/index.tsx
+++ apps/web/src/views/users/reviews/index.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import type { CommentaryProps } from '../../../types';
+import type { ReviewResponse } from '../../../types/review';
 
-import type { ReviewResponse } from '../../../types/user';
+import type { CommentaryResponse } from '../../../types/commentary';
 
 import { useAuth } from '../../../hooks/useAuth';
 
@@ -19,24 +19,44 @@ export interface UserMovieReviewProps {
 const UserMovieReview: React.FC<UserMovieReviewProps> = ({ review }) => {
   const { user } = useAuth();
 
-  const [commentaries, setCommentaries] = useState<CommentaryProps[]>([]);
+  const [commentaries, setCommentaries] = useState<CommentaryResponse[]>([]);
 
-  const handleComment = (message: string) => {
-    setCommentaries(prev => [...prev, { _id: 'abc', message }]);
+  const [likes, setLikes] = useState<string[]>([]);
+
+  const handleComment = (commentary: CommentaryResponse) => {
+    setCommentaries(prev => [...prev, commentary]);
   };
+
+  // TODO move commentary form to top
+
+  useEffect(() => {
+    setCommentaries(review.commentaries);
+  }, [review, setCommentaries]);
 
   return (
     <>
       <UserMovieReviewBody review={review} preview={false} />
 
       <div className="mt-4">
-        {user && (
-          <CommentaryForm onSubmit={values => handleComment(values.message)} />
-        )}
-
-        {commentaries.map(comm => (
-          <Commentary key={comm._id} />
+        {commentaries.map(commentary => (
+          <Commentary
+            key={commentary._id}
+            commentary={commentary}
+            liked={likes.includes(commentary._id)}
+            onLike={lod =>
+              !lod
+                ? setLikes(prev => prev.filter(c => c !== commentary._id))
+                : setLikes(prev => [...prev, commentary._id])
+            }
+          />
         ))}
+
+        {user && (
+          <CommentaryForm
+            referenceId={review._id}
+            onComment={commentary => handleComment(commentary)}
+          />
+        )}
       </div>
     </>
   );
