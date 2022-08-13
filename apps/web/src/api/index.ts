bd05340b6c35d86a7c82bfd913ecc8d818b77339
Victor <vhlima2000@gmail.com>
2022-08-12 23:30:46 -0300

    ✨ Authorization header is now being send via httpLinks from apollo

--- apps/web/src/api/index.ts
+++ apps/web/src/api/index.ts
@@ -1,7 +1,35 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/',
+});
+
+const authLink = setContext((_, { headers }) => {
+  if (typeof window === 'undefined') {
+    return { headers };
+  }
+
+  // get the authentication token from local storage if it exists
+
+  const token = localStorage.getItem('@MovieHouse:token');
+
+  // return the headers to the context so httpLink can read them
+
+  return {
+    headers: {
+      ...headers,
+
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 export default new ApolloClient({
   uri: 'http://localhost:4000/',
 
   cache: new InMemoryCache(),
+
+  link: authLink.concat(httpLink),
 });
