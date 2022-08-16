41c5edf3593f40f9992824f58f32936c036f3a78
Victor <vhlima2000@gmail.com>
2022-08-16 00:18:19 -0300

    🎨 Fetch reply count from commentaries

--- apps/server/src/entities/postgres/comment/commentary.interface.ts
+++ apps/server/src/entities/postgres/comment/commentary.interface.ts
@@ -1,6 +1,7 @@
-import { Field, Int, ObjectType } from 'type-graphql';
+import { Field, Int, ObjectType, Root } from 'type-graphql';
 
 import { Entity } from 'typeorm';
+import { ReplyRepository } from '../../../repositories';
 
 import BaseCommentary from './base.interface';
 
@@ -8,7 +9,11 @@ import BaseCommentary from './base.interface';
 @Entity('commentaries')
 export default class Commentary extends BaseCommentary {
   @Field(() => Int)
-  async replyCount() {
-    return 0;
+  async replyCount(@Root() commentary: Commentary) {
+    const count = await ReplyRepository.countBy({
+      commentaryId: commentary.id,
+    });
+
+    return count;
   }
 }
