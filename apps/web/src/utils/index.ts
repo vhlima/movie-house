e0c28aa65bcd623b46abdc2d41c886ab141ed970
Victor <vhlima2000@gmail.com>
2022-08-31 03:28:06 -0300

    ✨ Adds large numbers format to profile stats

--- apps/web/src/utils/index.ts
+++ apps/web/src/utils/index.ts
@@ -12,3 +12,19 @@ export const appendGql = (...docs: DocumentNode[]) => gql`
 
     ${docs.length > 3 ? docs[3] : ''}
   `;
+
+export const formatNumberToLargeScale = (num: number): string => {
+  const units = ['k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
+
+  let decimal = 0;
+
+  for (let i = units.length - 1; i >= 0; i -= 1) {
+    decimal = 1000 ** (i + 1);
+
+    if (num <= -decimal || num >= decimal) {
+      return +(num / decimal).toFixed(1) + units[i];
+    }
+  }
+
+  return String(num);
+};
--- apps/web/src/views/users/profile/components/ProfileStats/components/Stats/index.tsx
+++ apps/web/src/views/users/profile/components/ProfileStats/components/Stats/index.tsx
@@ -2,6 +2,8 @@ import Link from '../../../../../../../components/Link';
 
 import type { LinkProps } from '../../../../../../../components/Link';
 
+import { formatNumberToLargeScale } from '../../../../../../../utils';
+
 interface StatsProps {
   text: string;
   number: number;
@@ -13,7 +15,10 @@ const Stats: React.FC<StatsProps> = ({ number, text, link }) => (
     className="flex flex-col items-center border-r border-grey-700 px-2 first-of-type:pl-0 last-of-type:pr-0 last-of-type:border-0"
     {...link}
   >
-    <span className="text-grey-100 font-bold">{number}</span>
+    <span className="text-grey-100 font-bold">
+      {formatNumberToLargeScale(number)}
+    </span>
+
     <span className="text-grey-200">{text}</span>
   </Link>
 );
