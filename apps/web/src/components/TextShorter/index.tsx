b0f55c464688f18a396df3fe8a751f1eef5185fb
Victor <vhlima2000@gmail.com>
2022-08-31 00:17:51 -0300

    🎨 Text shorter implementation

--- apps/web/src/components/TextShorter/index.tsx
+++ apps/web/src/components/TextShorter/index.tsx
@@ -17,30 +17,36 @@ const TextShorter: React.FC<TextShorterProps> = ({
 }) => {
   const [isCollapsed, setCollapsed] = useState<boolean>(false);
 
+  const isMaxCharacters = text.length > maxCharacters;
+
   return (
     <p className={className && className}>
-      {!isCollapsed ? `${text.slice(0, maxCharacters)}...` : text}
+      {isCollapsed || !isMaxCharacters
+        ? text
+        : `${text.slice(0, maxCharacters)}...`}
 
-      <Button
-        buttonStyle="tertiary"
-        buttonSize="none"
-        full={false}
-        onClick={() => setCollapsed(prev => !prev)}
-      >
-        <div className="flex gap-1 items-center">
-          {!isCollapsed ? (
-            <>
-              <span>See more</span>
-              <SvgIcon iconType="FaChevronDown" />
-            </>
-          ) : (
-            <>
-              <span>See less</span>
-              <SvgIcon iconType="FaChevronUp" />
-            </>
-          )}
-        </div>
-      </Button>
+      {isMaxCharacters && (
+        <Button
+          buttonStyle="tertiary"
+          buttonSize="none"
+          full={false}
+          onClick={() => setCollapsed(prev => !prev)}
+        >
+          <div className="flex gap-1 items-center">
+            {!isCollapsed ? (
+              <>
+                <span>See more</span>
+                <SvgIcon iconType="FaChevronDown" />
+              </>
+            ) : (
+              <>
+                <span>See less</span>
+                <SvgIcon iconType="FaChevronUp" />
+              </>
+            )}
+          </div>
+        </Button>
+      )}
     </p>
   );
 };
