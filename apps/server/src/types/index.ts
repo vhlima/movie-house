954b09570b4a5ef1716a58cc0f69728a041c3b17
Victor <vhlima2000@gmail.com>
2022-09-16 10:42:52 -0300

    🏷️ Update type for User in server context types

--- apps/server/src/types/index.ts
+++ apps/server/src/types/index.ts
@@ -1,6 +1,6 @@
 import { DatasourceContext } from '../api';
 
-import User from '../entities/postgres/user.interface';
+import User from '../entities/pg-entities/user.interface';
 
 export interface ServerContext extends DatasourceContext {
   user?: User;
