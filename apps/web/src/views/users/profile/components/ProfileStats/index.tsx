ac5665e6e70831731c6278c3247f8ac6a4a7091f
Victor <vhlima2000@gmail.com>
2022-09-16 13:01:12 -0300

    💄 Show settings button on user profile

--- apps/web/src/views/users/profile/components/ProfileStats/index.tsx
+++ apps/web/src/views/users/profile/components/ProfileStats/index.tsx
@@ -37,7 +37,7 @@ const ProfileStats: React.FC = () => {
   return (
     <>
       <div className="flex gap-2 justify-center">
-        {isOwnProfile ? (
+        {!isOwnProfile ? (
           <>
             <FollowButton
               buttonSize="xs"
