1ed43801066227e5cb8a900815e6ba2ac37041a1
vhlima <vhlima2000@gmail.com>
2023-01-09 21:23:53 -0300

    🩹 Fix typing for return children on hooks component

--- apps/web/src/hooks/index.tsx
+++ apps/web/src/hooks/index.tsx
@@ -1,7 +1,8 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, ReactElement, ReactNode } from 'react';
 
 // import { AuthProvider } from './useAuth';
 
-const Hooks: React.FC<PropsWithChildren> = ({ children }) => children;
+const Hooks: React.FC<PropsWithChildren> = ({ children }) =>
+  children as ReactNode & ReactElement;
 
 export default Hooks;
