af5744c07b2ea776a02d2a122399186d43723cb7
vhlima <vhlima2000@gmail.com>
2023-01-22 14:26:47 -0300

    💄 Remove unused footer links and add transition colors

--- apps/web/src/Layout/components/Footer/index.tsx
+++ apps/web/src/Layout/components/Footer/index.tsx
@@ -1,34 +1,31 @@
 import Link from '../../../components/Link';
-
 import SvgIcon from '../../../components/SvgIcon';
 import Typography from '../../../components/Typography';
 
-const Footer: React.FC = () => {
-  const hoverStyle = 'hover:text-grey-300';
+const Footer: React.FC = () => (
+  <footer className="w-full flex flex-col items-center gap-4 p-4 mt-8 bg-grey-800">
+    <Link
+      className="font-semibold transition-colors text-grey-200 hover:text-grey-300"
+      href="/"
+    >
+      Contact
+    </Link>
 
-  return (
-    <footer className="flex flex-col items-center gap-4 p-4 mt-8 bg-grey-800 text-grey-200">
-      <div className="flex justify-center gap-2 font-semibold">
-        <Link className={hoverStyle} href="/">
-          Contact
-        </Link>
-      </div>
+    <Typography className="text-center" component="p" color="primary">
+      © Movie House 2023
+      <br />
+      Made by Victor Hugo Lima
+      <br />
+      All film data comes from TMDb
+    </Typography>
 
-      <Typography className="text-center" component="p" color="primary">
-        © Movie House 2023
-        <br />
-        Made by Victor Hugo Lima
-        <br />
-        All film data comes from TMDb
-      </Typography>
-
-      <div>
-        <Link className={hoverStyle} href="https://github.com/vhlima">
-          <SvgIcon iconType="FaGithub" size={28} />
-        </Link>
-      </div>
-    </footer>
-  );
-};
+    <Link
+      className="transition-colors text-grey-200 hover:text-grey-300"
+      href="https://github.com/vhlima"
+    >
+      <SvgIcon iconType="FaGithub" size={28} />
+    </Link>
+  </footer>
+);
 
 export default Footer;
