2d48b58097487673b56f8e985d604c604399c4be
vhlima <vhlima2000@gmail.com>
2023-01-22 14:32:21 -0300

    🎨 Add separate UserLink component

---
+++ apps/web/src/components/user/UserProfileLink/index.tsx
@@ -0,0 +1,28 @@
+import clsx from 'clsx';
+
+import type { PropsWithChildren } from 'react';
+
+import Link from '../../Link';
+
+interface UserProfileLinkProps {
+  className?: string;
+  username: string;
+}
+
+const UserProfileLink: React.FC<PropsWithChildren<UserProfileLinkProps>> = ({
+  className,
+  username,
+  children,
+}) => (
+  <Link
+    className={clsx(className && className)}
+    href={{
+      pathname: '/users/[username]',
+      query: { username },
+    }}
+  >
+    {children}
+  </Link>
+);
+
+export default UserProfileLink;
