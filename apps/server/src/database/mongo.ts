9769424c80f868eb87f9b9a430d0b0afb87aed0c
vhlima <vhlima2000@gmail.com>
2023-01-23 10:57:32 -0300

    🔧 Change mongo and typeorm versions to try connection with Railway

--- apps/server/src/database/mongo.ts
+++ apps/server/src/database/mongo.ts
@@ -3,18 +3,13 @@ import { DataSource } from 'typeorm';
 export const MongoDataSource = new DataSource({
   type: 'mongodb',
   url: process.env.MONGO_URL,
-  host:
-    process.env.NODE_ENV === 'development' ? undefined : process.env.MONGOHOST,
-  useNewUrlParser: true, // Needed for mongo
+  host: process.env.MONGOHOST,
+  useNewUrlParser: true,
   ssl: true,
   port: parseInt(process.env.MONGOPORT as string, 10),
   database: process.env.MONGO_DATABASE,
-  username:
-    process.env.NODE_ENV === 'development' ? undefined : process.env.MONGOUSER,
-  password:
-    process.env.NODE_ENV === 'development'
-      ? undefined
-      : process.env.MONGOPASSWORD,
+  username: process.env.MONGOUSER,
+  password: process.env.MONGOPASSWORD,
   synchronize: process.env.NODE_ENV === 'development',
   logging: true,
   entities:
