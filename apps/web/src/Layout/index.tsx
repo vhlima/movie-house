68bb409d120073966ef26022068e29f326fd35ae
vhlima <vhlima2000@gmail.com>
2023-02-03 17:49:23 -0300

    💄 Add margin bottom to layout main

--- apps/web/src/Layout/index.tsx
+++ apps/web/src/Layout/index.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => (
     <div className="flex flex-col relative w-screen h-screen overflow-x-hidden overflow-y-auto font-sans bg-grey-900">
       <Navbar />
 
-      <main className="relative w-full mx-auto max-w-5xl sm:px-3 md:px-9 mb-8">
+      <main className="relative w-full mx-auto max-w-5xl sm:px-3 md:px-9 mb-4">
         {children}
       </main>
 
