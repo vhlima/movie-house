d1ed5e17f48363f17df995599169547c09a32bdb
vhlima <vhlima2000@gmail.com>
2023-02-03 17:49:56 -0300

    💄 Implement new design on 404 page

--- apps/web/src/pages/404/index.tsx
+++ apps/web/src/pages/404/index.tsx
@@ -1,7 +1,26 @@
 import { NextPage } from 'next';
 
-import ErrorText from '../../components/ErrorText';
+import { FaSadTear } from 'react-icons/fa';
 
-const NotFoundPage: NextPage = () => <ErrorText text="Page not found" />;
+import Typography from '../../components/Typography';
+
+const NotFoundPage: NextPage = () => (
+  <div className="flex flex-col items-center p-12">
+    <FaSadTear className="text-grey-300" size={100} />
+
+    <Typography
+      className="font-bold text-center mt-6"
+      component="h1"
+      color="primary"
+      size="3xl"
+    >
+      404 Page Not Found
+    </Typography>
+
+    <Typography className="mt-3 text-center" component="p">
+      The page you are looking for doesn&apos;t exist or has been moved.
+    </Typography>
+  </div>
+);
 
 export default NotFoundPage;
