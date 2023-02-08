b4454a6c4d4690c9f15f713b505c37c5d50bcbf9
vhlima <vhlima2000@gmail.com>
2023-02-07 22:24:54 -0300

    🎨 Add SubHeading component

---
+++ apps/web/src/components/SubHeading/index.tsx
@@ -0,0 +1,24 @@
+import type { PropsWithChildren } from 'react';
+
+import Typography from '../Typography';
+
+interface SubHeadingProps {
+  title?: string;
+}
+
+const SubHeading: React.FC<PropsWithChildren<SubHeadingProps>> = ({
+  title,
+  children,
+}) => (
+  <div className="flex items-center gap-2 pb-1 border-b border-b-grey-800">
+    {title && (
+      <Typography className="uppercase" component="h2" size="sm">
+        {title}
+      </Typography>
+    )}
+
+    {children}
+  </div>
+);
+
+export default SubHeading;
--- apps/web/src/views/users/components/UserProfilePageView/index.tsx
+++ apps/web/src/views/users/components/UserProfilePageView/index.tsx
@@ -1,6 +1,7 @@
 import type { PropsWithChildren, ReactNode } from 'react';
 
 import Typography from '../../../../components/Typography';
+import SubHeading from '../../../../components/SubHeading';
 import PageContent from '../../../../components/PageContent';
 import BackdropImage from '../../../../components/BackdropImage';
 import ProfilePicture from '../../../../components/ProfilePicture';
@@ -21,54 +22,44 @@ interface UserProfilePageViewProps {
 
 const UserProfilePageView: React.FC<
   PropsWithChildren<UserProfilePageViewProps>
-> = ({ title, user, sortButtons, children }) => {
-  const a = 1;
+> = ({ title, user, sortButtons, children }) => (
+  <BackdropImage
+    src="https://a.ltrbxd.com/resized/sm/upload/dr/yl/7v/w5/its-a-wonderful-life-1200-1200-675-675-crop-000000.jpg"
+    alt="User profile backdrop"
+  >
+    <PageContent className="relative flex flex-col justify-center gap-8 w-full">
+      <div className="flex items-center gap-2">
+        <ProfilePicture imageSize="lg" src={user.profilePictureUrl} />
 
-  return (
-    <BackdropImage
-      src="https://a.ltrbxd.com/resized/sm/upload/dr/yl/7v/w5/its-a-wonderful-life-1200-1200-675-675-crop-000000.jpg"
-      alt="User profile backdrop"
-    >
-      <PageContent className="relative flex flex-col justify-center gap-8 w-full">
-        <div className="flex items-center gap-2">
-          <ProfilePicture imageSize="lg" src={user.profilePictureUrl} />
+        <div className="flex flex-grow flex-wrap items-center gap-x-2">
+          <Typography
+            className="font-bold"
+            component="h1"
+            color="primary"
+            size="2xl"
+          >
+            {user.username}
+          </Typography>
 
-          <div className="flex flex-grow flex-wrap items-center gap-x-2">
-            <Typography
-              className="font-bold"
-              component="h1"
-              color="primary"
-              size="2xl"
-            >
-              {user.username}
-            </Typography>
-
-            <div className="bg-movieHouse-mid rounded-md px-2">
-              <span className="text-white text-sm">Patron</span>
-            </div>
+          <div className="bg-movieHouse-mid rounded-md px-2">
+            <span className="text-white text-sm">Patron</span>
           </div>
         </div>
+      </div>
 
-        <ProfileStats user={user} />
+      <ProfileStats user={user} />
 
-        <ProfileNavigation user={user} />
+      <ProfileNavigation user={user} />
 
-        {(title || sortButtons) && (
-          <div className="flex items-center gap-2 border-b border-b-grey-800">
-            {title && (
-              <Typography className="uppercase" component="h1" size="sm">
-                {title}
-              </Typography>
-            )}
+      {(title || sortButtons) && (
+        <SubHeading title={title}>
+          {sortButtons && <div className="flex ml-auto">{sortButtons}</div>}
+        </SubHeading>
+      )}
 
-            {sortButtons && <div className="flex ml-auto">{sortButtons}</div>}
-          </div>
-        )}
-
-        {children}
-      </PageContent>
-    </BackdropImage>
-  );
-};
+      {children}
+    </PageContent>
+  </BackdropImage>
+);
 
 export default UserProfilePageView;
