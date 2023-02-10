5c2382b132d5ca108227a4b0a86af07feceb9777
vhlima <vhlima2000@gmail.com>
2023-02-10 01:11:29 -0300

    💄 Change close button color to red

--- apps/web/src/components/Modal/components/ModalCloseButton/index.tsx
+++ apps/web/src/components/Modal/components/ModalCloseButton/index.tsx
@@ -6,7 +6,7 @@ interface ModalCloseButtonProps {
 
 const ModalCloseButton: React.FC<ModalCloseButtonProps> = ({ onClose }) => (
   <button className="absolute right-0 top-0" type="button" onClick={onClose}>
-    <SvgIcon className="text-danger-light" iconType="FiX" size={28} />
+    <SvgIcon className="text-danger-base" iconType="FiX" size={28} />
   </button>
 );
 export default ModalCloseButton;
