56c2abf986cb7e37367a3030c1ae352ba5b64faf
vhlima <vhlima2000@gmail.com>
2023-02-10 05:54:10 -0300

    ✨ Redirect to movie page on search at movies page

--- apps/web/src/views/movies/components/MoviesPageView/index.tsx
+++ apps/web/src/views/movies/components/MoviesPageView/index.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+
 import type { PropsWithChildren } from 'react';
 
 import Input from '../../../../components/Input';
@@ -6,25 +8,37 @@ import MovieSearchInput from '../../../../components/movie/MovieSearchInput';
 
 import SortNavigation from './components/SortNavigation';
 
-const MoviesPageView: React.FC<PropsWithChildren> = ({ children }) => (
-  <div className="flex flex-col gap-8 mt-16">
-    <section className="flex flex-col items-center lg:flex-row">
-      <SortNavigation />
+const MoviesPageView: React.FC<PropsWithChildren> = ({ children }) => {
+  const { push } = useRouter();
 
-      <div className="mt-4 lg:mt-0 lg:ml-auto">
-        <Input.Label
-          className="flex flex-col lg:flex-row items-center uppercase text-sm gap-2"
-          text="Find a film"
-          htmlFor="searchMovie"
-          formik={false}
-        >
-          <MovieSearchInput dropdown onSelectMovie={() => ({})} />
-        </Input.Label>
-      </div>
-    </section>
+  return (
+    <div className="flex flex-col gap-8 mt-16">
+      <section className="flex flex-col items-center lg:flex-row">
+        <SortNavigation />
 
-    {children}
-  </div>
-);
+        <div className="mt-4 lg:mt-0 lg:ml-auto">
+          <Input.Label
+            className="flex flex-col lg:flex-row items-center uppercase text-sm gap-2"
+            text="Find a film"
+            htmlFor="searchMovie"
+            formik={false}
+          >
+            <MovieSearchInput
+              dropdown
+              onSelectMovie={movie =>
+                push({
+                  pathname: '/movies/[id]',
+                  query: { id: movie.id },
+                })
+              }
+            />
+          </Input.Label>
+        </div>
+      </section>
+
+      {children}
+    </div>
+  );
+};
 
 export default MoviesPageView;
