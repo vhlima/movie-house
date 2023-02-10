f1a60473655ab9fffe0e1147a7b70085891958b1
vhlima <vhlima2000@gmail.com>
2023-02-10 06:04:06 -0300

    💄 Improve styles on AddMovieToListModal

--- apps/web/src/components/movie/MovieActionsModal/components/AddMovieToListModal/index.tsx
+++ apps/web/src/components/movie/MovieActionsModal/components/AddMovieToListModal/index.tsx
@@ -39,55 +39,49 @@ const AddMovieToListModal: React.FC<AddMovieToListModalProps> = ({
       autoStyle={false}
       onClose={onClose}
     >
-      <div className="border-b border-b-grey-700">
-        <Button
-          className="p-2"
-          buttonStyle="tertiary"
-          buttonSize="none"
-          border={false}
-          rounded={false}
-          onClick={onClose}
-        >
-          <div className="flex items-center gap-2 mr-auto">
-            <SvgIcon className="text-grey-400" iconType="FaChevronLeft" />
+      <Button
+        className="p-2 border-b border-b-grey-700"
+        buttonStyle="tertiary"
+        buttonSize="none"
+        border={false}
+        rounded={false}
+        onClick={onClose}
+      >
+        <div className="flex items-center gap-2 mr-auto">
+          <SvgIcon className="text-grey-400" iconType="FaChevronLeft" />
 
-            <Typography className="font-bold" component="span" color="primary">
-              Back
-            </Typography>
-          </div>
-        </Button>
-      </div>
-
-      <div className="p-2">
-        <Input.Container styleType="secondary" borderFocus="none">
-          <Input
-            id="searchParams"
-            placeholder="Search"
-            formik={false}
-            onChange={e => setSearchParams(e.target.value)}
-          />
-
-          <Input.Icon iconType="FaSearch" size={18} direction="left" />
-        </Input.Container>
-      </div>
-
-      <div className="border-b border-b-grey-700">
-        <Button
-          className="flex gap-2 p-2 items-center"
-          buttonStyle="tertiary"
-          buttonSize="none"
-          flex={false}
-          border={false}
-          rounded={false}
-          onClick={() => setCreatingNewList(true)}
-        >
-          <SvgIcon iconType="BsPlusLg" />
-
-          <Typography component="span" color="secondary">
-            New list
+          <Typography className="font-bold" component="span" color="primary">
+            Back
           </Typography>
-        </Button>
-      </div>
+        </div>
+      </Button>
+
+      <Input.Container className="m-2" styleType="secondary" borderFocus="none">
+        <Input
+          id="searchParams"
+          placeholder="Search"
+          formik={false}
+          onChange={e => setSearchParams(e.target.value)}
+        />
+
+        <Input.Icon iconType="FaSearch" size={18} direction="left" />
+      </Input.Container>
+
+      <Button
+        className="flex gap-2 p-2 items-center border-b border-b-grey-700"
+        buttonStyle="tertiary"
+        buttonSize="none"
+        flex={false}
+        border={false}
+        rounded={false}
+        onClick={() => setCreatingNewList(true)}
+      >
+        <SvgIcon iconType="BsPlusLg" />
+
+        <Typography component="span" color="secondary">
+          New list
+        </Typography>
+      </Button>
 
       <UserLists
         filter={searchParams}
