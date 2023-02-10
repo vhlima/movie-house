e4d3236fe44830372e4efc3610dc86b0290d3411
vhlima <vhlima2000@gmail.com>
2023-02-10 05:39:44 -0300

    🎨 Add outside click to SortButton

--- apps/web/src/components/Sort/SortButton/index.tsx
+++ apps/web/src/components/Sort/SortButton/index.tsx
@@ -2,6 +2,8 @@ import clsx from 'clsx';
 
 import type { PropsWithChildren } from 'react';
 
+import { useOutsideClick } from '../../../hooks/useOutsideClick';
+
 import SvgIcon from '../../SvgIcon';
 import Typography from '../../Typography';
 
@@ -10,6 +12,7 @@ interface SortButtonProps {
   sizeType?: 'sm' | 'md' | 'lg';
   isOpen?: boolean;
   onClick: () => void;
+  onClose: () => void;
 }
 
 // TODO(BUG): When you fast click on the button the request
@@ -17,35 +20,42 @@ interface SortButtonProps {
 // because the component isn't being rendered.
 
 const SortButton: React.FC<PropsWithChildren<SortButtonProps>> = ({
-  text,
   sizeType = 'sm',
+  text,
   isOpen,
-  onClick,
   children,
-}) => (
-  <section
-    className={clsx('relative py-1', {
-      'bg-grey-800 rounded-t-sm': isOpen,
+  onClick,
+  onClose,
+}) => {
+  const { listRef, handleBlur } = useOutsideClick<HTMLSelectElement>();
 
-      'w-36 sm:w-28': sizeType === 'sm',
-      'w-36 sm:w-32': sizeType === 'md',
-      'w-36 sm:w-36': sizeType === 'lg',
-    })}
-  >
-    <button
-      className="w-full flex items-center justify-center"
-      type="button"
-      onClick={onClick}
+  return (
+    <section
+      className={clsx('relative py-1', {
+        'bg-grey-800 rounded-t-sm': isOpen,
+
+        'w-36 sm:w-28': sizeType === 'sm',
+        'w-36 sm:w-32': sizeType === 'md',
+        'w-36 sm:w-36': sizeType === 'lg',
+      })}
+      ref={listRef}
+      onBlur={e => handleBlur(e, onClose)}
     >
-      <Typography className="uppercase" component="span" size="sm">
-        {text}
-      </Typography>
+      <button
+        className="w-full flex items-center justify-center"
+        type="button"
+        onClick={onClick}
+      >
+        <Typography className="uppercase" component="span" size="sm">
+          {text}
+        </Typography>
 
-      <SvgIcon className="ml-2" iconType="FaChevronDown" />
-    </button>
+        <SvgIcon className="ml-2" iconType="FaChevronDown" />
+      </button>
 
-    {isOpen && children}
-  </section>
-);
+      {isOpen && children}
+    </section>
+  );
+};
 
 export default SortButton;
