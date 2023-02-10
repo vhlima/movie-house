608257c899514b3842a97f2ba0e12180a1e6be29
vhlima <vhlima2000@gmail.com>
2023-02-10 05:39:18 -0300

    🎨 Change GenreList list format

--- apps/web/src/components/Sort/GenreList/index.tsx
+++ apps/web/src/components/Sort/GenreList/index.tsx
@@ -6,27 +6,30 @@ interface GenreListProps {
 
 const GenreList: React.FC<GenreListProps> = ({ pathname }) => {
   const movieGenres = [
-    { id: '-1', name: 'Any genre' },
-    { id: '28', name: 'Action' },
-    { id: '12', name: 'Adventure' },
-    { id: '16', name: 'Animation' },
-    { id: '35', name: 'Comedy' },
-    { id: '80', name: 'Crime' },
-    { id: '99', name: 'Documentary' },
-    { id: '18', name: 'Drama' },
-    { id: '10751', name: 'Family' },
-    { id: '14', name: 'Fantasy' },
-    { id: '36', name: 'History' },
-    { id: '27', name: 'Horror' },
-    { id: '10402', name: 'Music' },
-    { id: '9648', name: 'Mystery' },
-    { id: '10749', name: 'Romance' },
-    { id: '878', name: 'Science Fiction' },
-    { id: '10770', name: 'TV Movie' },
-    { id: '53', name: 'Thriller' },
-    { id: '10752', name: 'War' },
-    { id: '37', name: 'Western' },
-  ];
+    'Any genre',
+    'Action',
+    'Adventure',
+    'Animation',
+    'Comedy',
+    'Crime',
+    'Documentary',
+    'Drama',
+    'Family',
+    'Fantasy',
+    'History',
+    'Horror',
+    'Music',
+    'Mystery',
+    'Romance',
+    'Science Fiction',
+    'TV Movie',
+    'Thriller',
+    'War',
+    'Western',
+  ].map((genre, index) => ({
+    id: index === 0 ? '-1' : genre.split(' ').join('-').toLowerCase(),
+    name: genre,
+  }));
 
   return (
     <SortDropdown
