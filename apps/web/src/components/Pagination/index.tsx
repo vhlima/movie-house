fe8d6dafe692bb5d9142420cb82ed83e3bee4006
vhlima <vhlima2000@gmail.com>
2023-03-07 19:45:18 -0300

    🎨 Improve pagination component

--- apps/web/src/components/Pagination/index.tsx
+++ apps/web/src/components/Pagination/index.tsx
@@ -1,42 +1,50 @@
-import clsx from 'clsx';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
 import Button from '../Button';
 
-import Link from '../Link';
 import Typography from '../Typography';
-import PaginationButton from './components/PaginationButton';
 
 interface Pagination {
-  path:
-    | string
-    | {
-        pathname: string;
-        query: Record<string, string>;
-      };
   currentPage: number;
   totalPages: number;
 }
 
-const Pagination: React.FC<Pagination> = ({
-  path,
-  currentPage,
-  totalPages,
-}) => {
-  const a = 1;
+const Pagination: React.FC<Pagination> = ({ currentPage, totalPages }) => {
+  const { push, pathname, query } = useRouter();
+
+  function handlePageChange(page: number) {
+    if (page < 1 || page > totalPages) return;
+
+    push({
+      pathname,
+      query: { ...query, page },
+    });
+  }
 
   return (
     <nav className="flex items-center justify-between">
-      <PaginationButton page={currentPage - 1} totalPages={totalPages}>
+      <Button
+        className="px-4"
+        buttonStyle="secondary"
+        full={false}
+        disabled={currentPage === 1}
+        onClick={() => handlePageChange(currentPage - 1)}
+      >
         Previous
-      </PaginationButton>
+      </Button>
 
       <Typography component="span">
         Page {currentPage} of {totalPages}
       </Typography>
 
-      <PaginationButton page={currentPage + 1} totalPages={totalPages}>
+      <Button
+        className="px-4"
+        buttonStyle="secondary"
+        full={false}
+        disabled={currentPage === totalPages}
+        onClick={() => handlePageChange(currentPage + 1)}
+      >
         Next
-      </PaginationButton>
+      </Button>
     </nav>
   );
 };
