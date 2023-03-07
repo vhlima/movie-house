e7f38d26faeac1365c7131fbb228ecb3be75f20c
vhlima <vhlima2000@gmail.com>
2023-03-07 01:11:56 -0300

    💄 Add margin bottom to sub heading

--- apps/web/src/components/SubHeading/index.tsx
+++ apps/web/src/components/SubHeading/index.tsx
@@ -10,7 +10,7 @@ const SubHeading: React.FC<PropsWithChildren<SubHeadingProps>> = ({
   title,
   children,
 }) => (
-  <div className="flex items-center gap-2 pb-1 border-b border-b-grey-800">
+  <div className="flex items-center gap-2 pb-1 border-b border-b-grey-800 mb-4">
     {title && (
       <Typography className="uppercase" component="h2" size="sm">
         {title}
