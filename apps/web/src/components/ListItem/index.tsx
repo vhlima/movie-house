d5064f7c420f97b037f2d36e319190354f0cbabd
vhlima <vhlima2000@gmail.com>
2023-03-15 04:27:37 -0300

    💄 Add option for lighter and darker border on ListItem

--- apps/web/src/components/ListItem/index.tsx
+++ apps/web/src/components/ListItem/index.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx';
 
 interface ListItemProps {
   className?: string;
+  borderColor?: 'light' | 'dark';
 }
 
 /* 
@@ -12,11 +13,16 @@ interface ListItemProps {
 
 export const ListItem: React.FC<PropsWithChildren<ListItemProps>> = ({
   className,
+  borderColor = 'dark',
   children,
 }) => (
   <li
     className={clsx(
-      'py-4 first-of-type:pt-0 last-of-type:pb-0 border-b border-b-grey-800 last-of-type:border-b-0',
+      'py-4 first-of-type:pt-0 last-of-type:pb-0 border-b last-of-type:border-b-0',
+      {
+        'border-b-grey-700': borderColor === 'light',
+        'border-b-grey-800': borderColor === 'dark',
+      },
       className,
     )}
   >
