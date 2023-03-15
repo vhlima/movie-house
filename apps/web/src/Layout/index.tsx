6bfab5a2c802faacf50d1a1c7c60692137c5f42c
vhlima <vhlima2000@gmail.com>
2023-03-15 18:46:00 -0300

    💄 Change layout padding size

--- apps/web/src/Layout/index.tsx
+++ apps/web/src/Layout/index.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => (
     <div className="flex flex-col relative w-screen h-screen overflow-x-hidden overflow-y-auto font-sans bg-grey-900">
       <Navbar />
 
-      <main className="relative w-full mx-auto max-w-5xl p-2 pt-0 md:p-4 md:pt-0">
+      <main className="relative w-full mx-auto max-w-5xl px-2 py-4 pt-0 md:px-4">
         {children}
       </main>
 
