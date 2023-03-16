2e2697fb057b70951b7433044a74c37648574d32
vhlima <vhlima2000@gmail.com>
2023-03-16 03:59:33 -0300

    ✨ Add MovieGenre navigation on movie page

--- apps/web/src/views/movies/movie/components/MovieGenres/index.tsx
+++ apps/web/src/views/movies/movie/components/MovieGenres/index.tsx
@@ -1,25 +1,35 @@
 import type { MovieGenre } from '@/graphql';
 
-import { Link } from '@/components';
+import { Button } from '@/components';
+import { useSortLinkBuilder } from '@/hooks/useSortLinkBuilder';
 
 interface MovieGenresProps {
   genres: MovieGenre[];
 }
 
-export const MovieGenres: React.FC<MovieGenresProps> = ({ genres }) => (
-  <ul className="flex gap-2 flex-wrap">
-    {genres.map(genre => (
-      <li
-        className="border rounded-md border-grey-700 transition-colors hover:bg-grey-600"
-        key={`movie-genre-${genre.id}`}
-        title={genre.name}
-      >
-        <Link href="/">
-          <h1 className="text-grey-100 whitespace-nowrap py-1 px-2">
+export const MovieGenres: React.FC<MovieGenresProps> = ({ genres }) => {
+  const { buildFilteredHref } = useSortLinkBuilder({
+    pathname: {
+      clean: '/movies',
+      sort: '/movies/genre/[genre]',
+    },
+    queryKey: 'genre',
+    singleOption: true,
+  });
+
+  return (
+    <ul className="flex gap-2 flex-wrap">
+      {genres.map(genre => (
+        <li className="border border-grey-700 rounded-md">
+          <Button
+            intent="tertiary"
+            size="sm"
+            href={buildFilteredHref(genre.name).href}
+          >
             {genre.name}
-          </h1>
-        </Link>
-      </li>
-    ))}
-  </ul>
-);
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
+};
