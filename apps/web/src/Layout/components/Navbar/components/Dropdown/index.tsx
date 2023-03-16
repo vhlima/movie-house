3f32453932f9fbb985b8b5c39474f8c63a37e362
vhlima <vhlima2000@gmail.com>
2023-03-16 02:06:32 -0300

    🎨 Improve navbar dropdown component

--- apps/web/src/Layout/components/Navbar/components/Dropdown/index.tsx
+++ apps/web/src/Layout/components/Navbar/components/Dropdown/index.tsx
@@ -1,29 +1,26 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 
 import type { PropsWithChildren } from 'react';
 
 import { useRouter } from 'next/router';
 
-import { MotionProps, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
+
+import { useOutsideClick } from '@/hooks/useOutsideClick';
 
 export interface DropdownProps {
+  tabIndex?: number;
   onClose: () => void;
 }
 
 const Dropdown: React.FC<PropsWithChildren<DropdownProps>> = ({
+  tabIndex,
   onClose,
   children,
 }) => {
   const { events } = useRouter();
 
-  const animation: MotionProps = useMemo(
-    () => ({
-      initial: { y: -15, opacity: 0 },
-      animate: { y: 0, opacity: 1 },
-      exit: { y: -15, opacity: 0 },
-    }),
-    [],
-  );
+  const { elementRef, handleBlur } = useOutsideClick<HTMLDivElement>();
 
   useEffect(() => {
     events.on('routeChangeStart', onClose);
@@ -33,9 +30,18 @@ const Dropdown: React.FC<PropsWithChildren<DropdownProps>> = ({
     };
   }, [events, onClose]);
 
+  useEffect(() => {
+    if (elementRef.current) {
+      elementRef.current.focus();
+    }
+  }, [elementRef]);
+
   return (
     <motion.div
-      className="absolute w-full p-3 bg-grey-800 rounded-b-md z-20 right-0 sm:left-0 top-14"
+      className="absolute w-full bg-grey-800 rounded-b-md z-20 right-0 sm:left-0 top-full overflow-hidden"
+      ref={elementRef}
+      tabIndex={tabIndex && tabIndex}
+      onBlur={e => handleBlur(e, onClose)}
       initial={{ y: -15, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       exit={{ y: -15, opacity: 0 }}
