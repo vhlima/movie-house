1a0268ac3df248dcf2644ae2c1db37618322b41b
vhlima <vhlima2000@gmail.com>
2023-03-16 03:27:51 -0300

    💄 Remove padding from layout main

--- apps/web/src/Layout/index.tsx
+++ apps/web/src/Layout/index.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => (
     <div className="flex flex-col relative w-screen h-screen overflow-x-hidden overflow-y-auto font-sans bg-grey-900">
       <Navbar />
 
-      <main className="relative w-full mx-auto max-w-5xl px-2 py-4 pt-0 md:px-4">
+      <main className="relative w-full mx-auto max-w-5xl py-4 pt-0">
         {children}
       </main>
 
