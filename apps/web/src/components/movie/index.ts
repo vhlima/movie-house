4046a73cf46b6d6f0ab09923df432fc0b09d5160
vhlima <vhlima2000@gmail.com>
2023-03-16 03:36:53 -0300

    ⚰️ Remove unused movie infos component

--- apps/web/src/components/movie/index.ts
+++ apps/web/src/components/movie/index.ts
@@ -1,7 +1,6 @@
 export * from './MovieActionsModal';
 export * from './MovieCover';
 export * from './MovieCoverList';
-export * from './MovieInfos';
 export * from './MovieLink';
 export * from './MovieRateModal';
 export * from './MovieSearchInput';
