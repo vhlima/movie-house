75a1ec38f469ea003f0671398246bd4fdb5dbb97
vhlima <vhlima2000@gmail.com>
2023-03-18 23:48:38 -0300

    💄 Add intent profile to user reviews on profile

--- apps/web/src/views/users/reviews/index.tsx
+++ apps/web/src/views/users/reviews/index.tsx
@@ -20,6 +20,7 @@ const UserReviewsView: React.FC<UserReviewsViewProps> = ({ user, reviews }) => {
       sortButtons={<ReviewsSortButtons user={user} />}
     >
       <ReviewList
+        intent="profile"
         reviews={reviews.edges.map(edge => edge.node)}
         emptyMessage="No reviews made yet."
       />
