e26c64ec039f70d10c7b56d9ac180eeb4f5f87d5
vhlima <vhlima2000@gmail.com>
2023-03-18 23:35:33 -0300

    💄 Remove display flex from card body

--- apps/web/src/components/Card/components/CardBody/index.tsx
+++ apps/web/src/components/Card/components/CardBody/index.tsx
@@ -1,7 +1,7 @@
 import type { PropsWithChildren } from 'react';
 
 const CardBody: React.FC<PropsWithChildren> = ({ children }) => (
-  <div className="flex flex-col gap-2 flex-grow">{children}</div>
+  <div className="flex flex-col gap-2">{children}</div>
 );
 
 export default CardBody;
