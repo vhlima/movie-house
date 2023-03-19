a7fda18b550fcbbadf0485c2f28d78bfd70fce4d
vhlima <vhlima2000@gmail.com>
2023-03-19 06:05:20 -0300

    💄 Remove unused styles from SortListItem

--- apps/web/src/views/movies/components/MoviesPageView/components/SortNavigation/components/SortListItem/index.tsx
+++ apps/web/src/views/movies/components/MoviesPageView/components/SortNavigation/components/SortListItem/index.tsx
@@ -1,44 +1,9 @@
 import type { PropsWithChildren } from 'react';
 
-import { useOutsideClick } from '@/hooks/useOutsideClick';
-
-import { Typography, SvgIcon } from '@/components';
-
-interface SortListItemProps {
-  text: string;
-  onClick: () => void;
-  onClose: () => void;
-}
-
-const SortListItem: React.FC<PropsWithChildren<SortListItemProps>> = ({
-  text,
-  onClick,
-  onClose,
-  children,
-}) => {
-  const { elementRef, handleBlur } = useOutsideClick<HTMLLIElement>();
-
-  return (
-    <li
-      className="relative bg-grey-800 border-r border-r-grey-700 last:border-r-0"
-      ref={elementRef}
-      onBlur={e => handleBlur(e, onClose)}
-    >
-      <button
-        className="flex items-center gap-1 w-full px-4 lg:px-3 py-0.5"
-        type="button"
-        onClick={onClick}
-      >
-        <Typography className="uppercase" component="span" size="sm">
-          {text}
-        </Typography>
-
-        <SvgIcon className="flex-shrink-0" iconType="FaChevronDown" />
-      </button>
-
-      {children}
-    </li>
-  );
-};
+const SortListItem: React.FC<PropsWithChildren> = ({ children }) => (
+  <li className="bg-grey-800 border-r border-r-grey-700 last:border-r-0">
+    {children}
+  </li>
+);
 
 export default SortListItem;
