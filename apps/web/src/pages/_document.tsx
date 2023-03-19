c0d8dfb3dcd907c37a878cfc3e355dc647d131a0
vhlima <vhlima2000@gmail.com>
2023-03-19 02:52:39 -0300

    🔧 Remove font 600 and add 500

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           />
 
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;600;700&family=Roboto:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
 
