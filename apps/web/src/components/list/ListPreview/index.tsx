8f859626faa01b03424279db81bad89c4dffbc84
vhlima <vhlima2000@gmail.com>
2023-03-19 04:22:04 -0300

    💄 Improve list preview stylings

--- apps/web/src/components/list/ListPreview/index.tsx
+++ apps/web/src/components/list/ListPreview/index.tsx
@@ -1,76 +1,110 @@
-import type { FindListsQuery } from '@/graphql';
+import clsx from 'clsx';
 
-import { Link, Typography, SvgIcon, TextShorter } from '@/components';
-import { MovieCoverList } from '@/components/movie';
+import { FindListsQuery, LikeType } from '@/graphql';
 
-interface ListPreviewProps {
+import {
+  Link,
+  Typography,
+  SvgIcon,
+  TextShorter,
+  ListItem,
+  LikeButton,
+  ProfilePicture,
+} from '@/components';
+
+import { MovieCoverAccordion } from '@/components/movie';
+
+import UserProfileLink from '@/components/user/UserProfileLink';
+
+interface Props {
   list: FindListsQuery['lists']['edges'][number]['node'];
+  showUser?: boolean;
 }
 
-const ListPreview: React.FC<ListPreviewProps> = ({ list }) => {
-  const { id, name, post, movies } = list;
+const ListPreview: React.FC<Props> = ({ list, showUser = true }) => {
+  const { id, name, user, post, movies } = list;
 
   return (
-    <div>
+    <ListItem className="md:flex md:gap-4">
       <Link
-        className="group"
+        className="block md:flex-shrink-0"
         href={{
           pathname: '/lists/[id]',
           query: { id },
         }}
       >
-        {movies.length > 0 && (
-          <MovieCoverList
-            name="list-preview-movies"
-            movies={movies}
-            empty={4 - movies.length}
-            link={false}
+        <MovieCoverAccordion
+          className="w-full"
+          size="sm"
+          movies={movies}
+          maxAmount={5}
+        />
+      </Link>
+
+      <div className="w-full">
+        <Typography
+          className="font-bold"
+          component="h2"
+          color="primary"
+          size="lg"
+          hover
+        >
+          <Link
+            className="block"
+            href={{
+              pathname: '/lists/[id]',
+              query: { id },
+            }}
+          >
+            {name}
+          </Link>
+        </Typography>
+
+        <Typography component="span" color="tertiary" size="sm">
+          {movies.length} {movies.length === 1 ? 'movie' : 'movies'}
+        </Typography>
+
+        {showUser && (
+          <UserProfileLink
+            className="flex items-center gap-2 group mt-2"
+            username={user.username}
+          >
+            <ProfilePicture src={user.profilePictureUrl} imageSize="sm" />
+
+            <Typography className="font-bold" component="span" groupHover>
+              {user.username}
+            </Typography>
+          </UserProfileLink>
+        )}
+
+        {post.content && (
+          <TextShorter
+            className="mt-2"
+            text={post.content}
+            maxCharacters={120}
           />
         )}
 
-        <Typography
-          className="font-semibold group-hover:text-grey-200"
-          component="h1"
-          color="primary"
-          size="lg"
+        <div
+          className={clsx('flex gap-2 items-center ml-auto mt-2', {
+            'mt-4': !post.content && showUser,
+          })}
         >
-          {name}
-        </Typography>
-      </Link>
-
-      <div className="flex items-center gap-1">
-        <SvgIcon iconType="TbMovie" size={24} />
-
-        <Typography component="span" color="primary">
-          {movies.length} {movies.length === 1 ? 'movie' : 'movies'}
-        </Typography>
-      </div>
-
-      {post.content && (
-        <TextShorter
-          className="text-grey-200 mt-1"
-          text={post.content}
-          maxCharacters={120}
-        />
-      )}
-
-      {/* <div className="flex gap-2 items-center mt-1 ml-auto">
-        <div className="flex items-center gap-1">
-          <SvgIcon className="text-grey-300" iconType="AiFillHeart" size={24} />
-
-          <Typography component="span">3.6k</Typography>
-        </div>
-
-        <div className="flex items-center gap-1">
-          <SvgIcon
-            className="text-grey-300"
-            iconType="BsFillChatLeftFill"
-            size={18}
+          <LikeButton
+            className="w-16"
+            contentId={post.id}
+            likeType={LikeType.Post}
           />
-          <Typography component="span">3.6k</Typography>
+
+          <button className="flex items-center gap-1 w-fit" type="button">
+            <SvgIcon iconType="HiChatBubbleBottomCenter" size={22} />
+            <Typography className="font-medium" component="span" size="sm">
+              3.6k
+            </Typography>
+          </button>
         </div>
-      </div> */}
-    </div>
+      </div>
+    </ListItem>
   );
 };
 
