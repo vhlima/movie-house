6919f4ae71440376fdbd685a6bf84bdb72b73d25
vhlima <vhlima2000@gmail.com>
2023-03-20 08:47:35 -0300

    💄 Remove some base stylings and add className to SubHeading

--- apps/web/src/components/SubHeading/index.tsx
+++ apps/web/src/components/SubHeading/index.tsx
@@ -4,19 +4,25 @@ import type { PropsWithChildren } from 'react';
 import { Typography } from '@/components';
 
 interface SubHeadingProps {
+  className?: string;
   title?: string;
   marginBottom?: boolean;
 }
 
 export const SubHeading: React.FC<PropsWithChildren<SubHeadingProps>> = ({
+  className,
   title,
   marginBottom,
   children,
 }) => (
   <div
-    className={clsx('flex items-center gap-2 pb-1 border-b border-b-grey-800', {
-      'mb-4': marginBottom,
-    })}
+    className={clsx(
+      'flex gap-2 pb-2 border-b border-b-grey-800',
+      {
+        'mb-4': marginBottom,
+      },
+      className && className,
+    )}
   >
     {title && (
       <Typography className="uppercase" component="h2" size="sm">
