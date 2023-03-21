ab71bd13f5ba7044c664e4ff0c0f700388073e3e
vhlima <vhlima2000@gmail.com>
2023-03-21 00:40:56 -0300

    ✨ Implement new list view

--- apps/web/src/views/lists/index.tsx
+++ apps/web/src/views/lists/index.tsx
@@ -1,12 +1,23 @@
-import { FindListQuery } from '@/graphql';
+import { useRouter } from 'next/router';
+
+import { FindListQuery, LikeType } from '@/graphql';
 
 import {
   Typography,
   PageContent,
   PostCommentaries,
   ProfilePicture,
+  SubHeading,
+  TextShorter,
+  LikeButton,
+  SvgIcon,
 } from '@/components';
-import { formatDateDistanceFromMillis } from '../../utils/date-utils';
+
+import { DecadeDropdown, GenreDropdown, SortButton } from '@/components/Sort';
+
+import { SingleDropdown } from '@/hooks/useSingleDropdown';
+
+import { formatDateDistanceFromMillis } from '@/utils/date-utils';
 
 import BackdropImage from '../../components/BackdropImage';
 
@@ -17,52 +28,51 @@ import ListMovies from './components/ListMovies';
 type UserListViewProps = FindListQuery;
 
 const UserListView: React.FC<UserListViewProps> = ({ list }) => {
+  const { asPath } = useRouter();
+
+  const rootPath = asPath.split('/').slice(0, 4).join('/');
+
   const bgUrl =
-    'https://a.ltrbxd.com/resized/sm/upload/es/4u/du/em/spooky-1200-1200-675-675-crop-000000.jpg?v=4f77fabd8c';
+    'https://a.ltrbxd.com/resized/sm/upload/0y/9x/ts/cw/speed-racer-1200-1200-675-675-crop-000000.jpg';
 
   const { id: listId, user, name, post } = list;
 
+  const postCreationDateFormatted = formatDateDistanceFromMillis(
+    post.createdAt,
+  );
+
   return (
     <BackdropImage src={bgUrl} alt="Backdrop image for user list">
-      <PageContent className="flex flex-col gap-2 relative">
-        <div className="flex items-center">
-          <ProfilePicture src={user.profilePictureUrl} imageSize="sm" />
+      <PageContent className="flex flex-col relative">
+        <SubHeading className="flex-col sm:flex-row">
+          <UserProfileLink
+            className="flex items-center group"
+            username={user.username}
+          >
+            <ProfilePicture src={user.profilePictureUrl} imageSize="sm" />
 
-          <Typography className="ml-2 mr-1" component="span">
-            List by
-          </Typography>
-
-          <UserProfileLink username={user.username}>
-            <Typography
-              className="hover:text-grey-200"
-              component="strong"
-              color="primary"
-            >
-              {user.username}
+            <Typography className="ml-2" component="span" groupHover>
+              List by&nbsp;
+              <Typography component="strong" color="primary" groupHover>
+                {user.username}
+              </Typography>
             </Typography>
           </UserProfileLink>
-        </div>
 
-        <div className="flex items-center">
-          <Typography size="sm" component="span">
-            Published
-            <Typography className="ml-1" component="time">
-              {formatDateDistanceFromMillis(post.createdAt)}
-            </Typography>
-          </Typography>
+          <div className="flex flex-col items-center gap-2 flex-wrap sm:gap-0 sm:flex-nowrap sm:flex-row sm:w-fit sm:ml-auto">
+            <SingleDropdown>
+              <SortButton type="decade" intent="secondary">
+                <DecadeDropdown pathname={rootPath} />
+              </SortButton>
 
-          {/* <div className="flex items-center ml-auto">
-            <button type="button">
-              <SvgIcon
-                className="text-grey-300 hover:text-grey-400"
-                size={24}
-                iconType="MdOutlineSort"
-              />
-            </button>
-          </div> */}
-        </div>
+              <SortButton type="genre" intent="secondary">
+                <GenreDropdown pathname={rootPath} />
+              </SortButton>
+            </SingleDropdown>
+          </div>
+        </SubHeading>
 
-        <section>
+        <section className="mt-2">
           <Typography
             className="font-semibold"
             component="h1"
@@ -72,18 +82,31 @@ const UserListView: React.FC<UserListViewProps> = ({ list }) => {
             {name}
           </Typography>
 
+          <Typography component="span" size="sm" color="tertiary">
+            Published&nbsp;{postCreationDateFormatted}
+          </Typography>
+
           {post.content && (
-            <Typography component="p">{post.content}</Typography>
+            <TextShorter
+              className="my-2"
+              text={post.content}
+              maxCharacters={300}
+            />
           )}
 
           <ListMovies listId={listId} />
+
+          <div className="flex gap-2 my-4">
+            <LikeButton contentId={list.post.id} likeType={LikeType.Post} />
+
+            <div className="flex items-center gap-1 w-fit">
+              <SvgIcon iconType="HiChatBubbleBottomCenter" size={22} />
+              <Typography className="font-medium" component="span" size="sm">
+                3.6k
+              </Typography>
+            </div>
+          </div>
         </section>
-
-        {/* <div className="flex gap-2 mb-2">
-          <LikeButton rootId={list.post.id} likeCount={0} />
-
-          <CommentaryCount count={0} />
-        </div> */}
 
         <PostCommentaries postId={post.id} />
       </PageContent>
