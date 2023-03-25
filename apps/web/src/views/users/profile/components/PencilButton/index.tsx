ed90c0a78c42d3845247a81eea80279824246d42
vhlima <vhlima2000@gmail.com>
2023-03-25 02:20:11 -0300

    🎨 Add logic for conditional rendering PencilButton

--- apps/web/src/views/users/profile/components/PencilButton/index.tsx
+++ apps/web/src/views/users/profile/components/PencilButton/index.tsx
@@ -1,13 +1,27 @@
 import { ButtonHTMLAttributes } from 'react';
 
 import { SvgIcon } from '@/components';
+import { useAuth } from '@/hooks/useAuth';
+import { useProfile } from '@/views/users/hooks/useProfile';
 
 type PencilButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-const PencilButton: React.FC<PencilButtonProps> = ({ ...props }) => (
-  <button className="ml-auto" type="button" {...props}>
-    <SvgIcon iconType="FaPencilAlt" />
-  </button>
-);
+const PencilButton: React.FC<PencilButtonProps> = ({ ...props }) => {
+  const { data: session } = useAuth();
+
+  const { user } = useProfile();
+
+  const isSameUserAsSession = session && session.user.id === user?.id;
+
+  if (!session || !isSameUserAsSession) {
+    return null;
+  }
+
+  return (
+    <button className="ml-auto" type="button" {...props}>
+      <SvgIcon iconType="FaPencilAlt" />
+    </button>
+  );
+};
 
 export default PencilButton;
