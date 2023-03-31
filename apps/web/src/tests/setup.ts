2a79e9984ccd4b3c2a8fcb4efbeabfd9c0d5ac10
vhlima <vhlima2000@gmail.com>
2023-03-31 09:06:54 -0300

    🎨 Add next router compatibility on tests

--- apps/web/src/tests/setup.ts
+++ apps/web/src/tests/setup.ts
@@ -1,2 +1,8 @@
 /* eslint-disable */
 import '@testing-library/jest-dom';
+
+import { createContext } from 'react';
+
+jest.mock('next/dist/shared/lib/router-context.js', () => ({
+  RouterContext: createContext(true),
+}));
