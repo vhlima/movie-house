40e28276be9b8babeb6604b4d2af408c270b0b99
vhlima <vhlima2000@gmail.com>
2023-04-04 09:37:21 -0300

    🎨 Add UserIdentity to PostMeta

--- apps/web/src/components/PostMeta/index.tsx
+++ apps/web/src/components/PostMeta/index.tsx
@@ -2,20 +2,33 @@ import { TextShorter } from '@/components';
 
 import { PostReactions } from './components';
 
+import { UserIdentity } from '../user';
+
 interface Props {
   id: string;
   content?: string;
   commentaryCount: number;
+  user?: {
+    username: string;
+    profilePictureUrl?: string;
+  };
 }
 
 export const PostMeta: React.FC<Props> = props => {
-  const { id, content, commentaryCount } = props;
+  const { id, user, content, commentaryCount } = props;
 
   return (
     <div className="mt-4">
+      {user && (
+        <UserIdentity
+          username={user.username}
+          profilePictureUrl={user.profilePictureUrl}
+        />
+      )}
+
       {content && (
         <TextShorter
-          className="mb-4"
+          className="my-4"
           maxCharacters={200}
           text={content}
           data-testid="post-meta-content"
