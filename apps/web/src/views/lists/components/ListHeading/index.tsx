1b00f814622a58698dbb725e50e1c44bc4c9aafe
vhlima <vhlima2000@gmail.com>
2023-04-04 09:39:49 -0300

    🎨 Improve ListHeading with single responsibility

--- apps/web/src/views/lists/components/ListHeading/index.tsx
+++ apps/web/src/views/lists/components/ListHeading/index.tsx
@@ -1,49 +1,21 @@
-import { Typography, ProfilePicture, SubHeading } from '@/components';
+import { SubHeading } from '@/components';
 
-import { DecadeDropdown, GenreDropdown, SortButton } from '@/components/Sort';
-
-import { SingleDropdown } from '@/hooks/useSingleDropdown';
-
-import UserProfileLink from '@/components/user/UserProfileLink';
+import { ListInfo, ListMoviesSortButtons } from './components';
 
 interface Props {
-  listId: string;
-  user: {
-    username: string;
-    profilePictureUrl?: string;
-  };
+  id: string;
+  name: string;
+  createdAt: number;
 }
 
-export const ListHeading: React.FC<Props> = ({ listId, user }) => {
-  const rootPath = `/lists/${listId}`;
+export const ListHeading: React.FC<Props> = props => {
+  const { id, name, createdAt } = props;
 
   return (
-    <SubHeading className="flex-col sm:flex-row">
-      <UserProfileLink
-        className="flex items-center group"
-        username={user.username}
-      >
-        <ProfilePicture src={user.profilePictureUrl} imageSize="sm" />
+    <SubHeading className="flex flex-col sm:flex-row gap-0 pb-2">
+      <ListInfo name={name} createdAt={createdAt} />
 
-        <Typography className="ml-2" component="span" groupHover>
-          List by&nbsp;
-          <Typography component="strong" color="primary" groupHover>
-            {user.username}
-          </Typography>
-        </Typography>
-      </UserProfileLink>
-
-      <div className="flex flex-col items-center gap-2 flex-wrap sm:gap-0 sm:flex-nowrap sm:flex-row sm:w-fit sm:ml-auto">
-        <SingleDropdown>
-          <SortButton type="decade" intent="secondary">
-            <DecadeDropdown pathname={rootPath} />
-          </SortButton>
-
-          <SortButton type="genre" intent="secondary">
-            <GenreDropdown pathname={rootPath} />
-          </SortButton>
-        </SingleDropdown>
-      </div>
+      <ListMoviesSortButtons listId={id} />
     </SubHeading>
   );
 };
