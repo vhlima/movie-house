5160aaa9a64484d0b67ed5fecffb8f870d615ec9
vhlima <vhlima2000@gmail.com>
2023-04-04 08:41:28 -0300

    🎨 Implement PostReactions on PostMeta

--- apps/web/src/components/PostMeta/index.tsx
+++ apps/web/src/components/PostMeta/index.tsx
@@ -1,20 +1,28 @@
-import { PostReactions, TextShorter } from '@/components';
+import { TextShorter } from '@/components';
+
+import { PostReactions } from './components';
 
 interface Props {
   id: string;
   content?: string;
+  commentaryCount: number;
 }
 
 export const PostMeta: React.FC<Props> = props => {
-  const { id, content } = props;
+  const { id, content, commentaryCount } = props;
 
   return (
     <div className="mt-4">
       {content && (
-        <TextShorter className="mb-4" maxCharacters={200} text={content} />
+        <TextShorter
+          className="mb-4"
+          maxCharacters={200}
+          text={content}
+          data-testid="post-meta-content"
+        />
       )}
 
-      <PostReactions postId={id} />
+      <PostReactions postId={id} commentaryCount={commentaryCount} />
     </div>
   );
 };
