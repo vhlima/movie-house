f1ec654117fed4d303d63f625deba60f33df1aa6
vhlima <vhlima2000@gmail.com>
2023-04-05 11:52:26 -0300

    ✅ Ensure movie actor original name is being rendered correctly

---
+++ apps/web/src/views/movies/movie/components/MovieCast/components/MovieActor/index.spec.tsx
@@ -0,0 +1,40 @@
+import { RenderResult, render } from '@testing-library/react';
+
+import { faker } from '@faker-js/faker';
+
+import { MovieActor } from '.';
+
+type SutType = {
+  sut: RenderResult;
+  originalName: string;
+  character: string;
+};
+
+function createSut(): SutType {
+  const originalName = faker.name.fullName();
+  const character = faker.name.fullName();
+
+  const sut = render(
+    <MovieActor
+      id={faker.datatype.number()}
+      character={character}
+      originalName={originalName}
+    />,
+  );
+
+  return {
+    sut,
+    originalName,
+    character,
+  };
+}
+
+describe('MovieActor', () => {
+  test('Should render originalName correctly', () => {
+    const { sut, originalName } = createSut();
+
+    const originalNameElement = sut.getByTestId('actor-original-name');
+    expect(originalNameElement).toBeInTheDocument();
+    expect(originalNameElement.textContent).toEqual(originalName);
+  });
+});
