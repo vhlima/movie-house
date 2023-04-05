2a8eb05c65747d7183dfb06507c6375ce8f82cbf
vhlima <vhlima2000@gmail.com>
2023-04-05 11:37:52 -0300

    🎨 Add MovieActorList component

---
+++ apps/web/src/views/movies/movie/components/MovieCast/components/MovieActorList/index.tsx
@@ -0,0 +1,36 @@
+import { Typography } from '@/components';
+
+import { MovieActor } from '../index';
+
+interface Props {
+  actors: Array<{
+    id: number;
+    character: string;
+    originalName: string;
+    profilePictureUrl?: string;
+  }>;
+}
+
+export const MovieActorList: React.FC<Props> = props => {
+  const { actors } = props;
+
+  if (actors.length === 0) {
+    <Typography component="h2" data-testid="empty-list-message">
+      The cast for this film was not found.
+    </Typography>;
+  }
+
+  return (
+    <ul className="flex overflow-x-auto">
+      {actors.map(actor => (
+        <MovieActor
+          key={`movie-actor-${actor.id}`}
+          id={actor.id}
+          character={actor.character}
+          originalName={actor.originalName}
+          profilePictureUrl={actor.profilePictureUrl}
+        />
+      ))}
+    </ul>
+  );
+};
--- apps/web/src/views/movies/movie/components/MovieCast/components/index.ts
+++ apps/web/src/views/movies/movie/components/MovieCast/components/index.ts
@@ -1,1 +1,2 @@
 export * from './MovieActor';
+export * from './MovieActorList';
