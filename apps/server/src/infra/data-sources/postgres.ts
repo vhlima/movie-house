72fb7f51a570be0e1a82c14388160cd177463a58
vhlima <vhlima2000@gmail.com>
2023-04-25 19:51:01 -0300

    🔥 Remove postgres connection by url

--- apps/server/src/infra/data-sources/postgres.ts
+++ apps/server/src/infra/data-sources/postgres.ts
@@ -2,7 +2,6 @@ import { DataSource } from 'typeorm';
 
 export const PostgresDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.PGHOST,
   port: parseInt(process.env.PGPORT as string, 10),
   username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
